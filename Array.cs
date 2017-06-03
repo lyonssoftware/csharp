4485d543e81bab2ea3a330e0767e51ae4b49d46e
Lyons Software <admin@lyonssoftware.com>
2017-06-02 23:15:30 -0500

    Update Array.cs

--- Array.cs
+++ Array.cs
@@ -3,7 +3,7 @@ public class Array
    public static void Main()
    {
      // create an int array 
-     int[] array = new int[17];
+     int[] array = new int[19];
       array[0] = 1;
       array[1] = 2;
       array[2] = 3;
@@ -21,8 +21,8 @@ public class Array
       array[14] = 15;
       array[15] = 16;
       array[16] = 17;
-      array[16] = 18;
-      
+      array[17] = 18;
+      array[18] = 19;
       
       Console.WriteLine(array[0]);
       Console.WriteLine(array[1]);
