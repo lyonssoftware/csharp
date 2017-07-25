57f28e58a5fe63bc068b85ca5591552725865402
Lyons Software <admin@lyonssoftware.com>
2017-07-24 23:41:45 -0500

    Update Subtraction.cs

--- Subtraction.cs
+++ Subtraction.cs
@@ -5,9 +5,11 @@ public class Subtraction
     int a = 5;
     int b = 3;
     int c = 4;
+    int d = 7;
 
     Console.WriteLine(a-b);
     Console.writeline(b-a);
     Console.WriteLine(a-c);
+    Console.WriteLine(d-c);
   }
 }
