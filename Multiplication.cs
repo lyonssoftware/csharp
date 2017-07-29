1f2815f0c13f8eac2f6c14dd7e522f082dbf6b01
Lyons Software <admin@lyonssoftware.com>
2017-07-28 21:55:31 -0500

    Update Multiplication.cs

--- Multiplication.cs
+++ Multiplication.cs
@@ -1,10 +1,11 @@
 Class Multiplication
-
-public static void Main()
 {
-    int x = 3;
-    int 6 = 8;
+    public static void Main()
+    {
+        int x = 3;
+        int y = 8;
+        int z = -1
     
-  Console.WriteLine(x*y);
-}
+        Console.WriteLine(x*y);
+    }
 }
