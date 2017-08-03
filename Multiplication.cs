198fa29bb608ec373792c82c9b46adc0d5b5ba94
Lyons Software <admin@lyonssoftware.com>
2017-08-02 22:35:39 -0500

    Update Multiplication.cs

--- Multiplication.cs
+++ Multiplication.cs
@@ -7,7 +7,9 @@ Class Multiplication
         int z = -1
         int w = 6;
         int i = 8;
-    
+        int a = 4;
+        int b = x;
+        
         Console.WriteLine(x*y);
         Console.WriteLine(x*z);
         Console.WriteLine(x*w);
