8b3bc4c0ec141a958bcf62f98b9ab2679fc18d7b
Lyons Software <admin@lyonssoftware.com>
2017-08-16 23:54:29 -0500

    Update Division.cs

--- Division.cs
+++ Division.cs
@@ -4,8 +4,11 @@ Class Division
     {
         int x = 3;
         int y = 8;
+        int c = 10;
+        int d = 3;
        
         Console.WriteLine(x/y);
         Console.WriteLine(y/x);
+        Console.WriteLine(x/d);
     }
 }
