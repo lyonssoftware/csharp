5a4c0c90ff32466d66c99419a700335da1cd5a40
Lyons Software <admin@lyonssoftware.com>
2017-09-27 23:40:35 -0500

    Update HelloWorld.cs

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -2,6 +2,7 @@ public class HelloWord
 {
    public static void Main()
    {
+      // this is a comment 
 System.Console.WriteLine("Hello, World!");
       System.Console.WriteLine("My name is Bob.");
       
