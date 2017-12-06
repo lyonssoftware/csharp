d453c4284e9fd7b9fd81180e66081349a4b6a6e9
Lyons Software <admin@lyonssoftware.com>
2017-12-05 23:34:22 -0600

    Update ForLoop.cs

--- ForLoop.cs
+++ ForLoop.cs
@@ -4,7 +4,13 @@ class ForLoop
   {
     for(int i = 0; i < 10; i++)
     {
-       Console.WriteLine("The is a for loop.");
+       Console.WriteLine("The is an increment for loop.");
+    }
+    
+    // decrement
+    for(int i = 10; i > 0; i--)
+    {
+      Console.WriteLine("The is a decrement for loop.");
     }
   }
 }
