4e88d219d07bb0db8bd9227441ea733a33c3fb5b
Lyons Software <admin@lyonssoftware.com>
2017-12-11 23:05:29 -0600

    Create WhileLoop.cs

---
+++ WhileLoop.cs
@@ -0,0 +1,13 @@
+public class WhileLoop
+{
+   public static void Main()
+   {
+      // this is a comment 
+      int i = 5;
+      while(i > 0)
+      {
+         System.Console.WriteLine("The current count is greater than 0.");
+         i--;
+      }
+   }
+}
