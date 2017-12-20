a9d3cdb4ba9fb3ed2e48b0e2a83d35b1ec81ade9
Lyons Software <admin@lyonssoftware.com>
2017-12-19 23:17:51 -0600

    Create DoWhile.cs

---
+++ DoWhile.cs
@@ -0,0 +1,17 @@
+public class DoWhile
+{
+   // while loops 
+   public static void Main()
+   {
+    int x = 3;
+      do 
+      {
+        x++;
+      }
+      
+     while(x < 10)
+     {
+        
+     }
+   }
+}
