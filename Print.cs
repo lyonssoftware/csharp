08e822178071b95801d60b4317f76096f0c1d53a
Lyons Software <admin@lyonssoftware.com>
2018-01-10 23:38:16 -0800

    Update Print.cs

--- Print.cs
+++ Print.cs
@@ -25,38 +25,7 @@ public class Print
       // this is the last comment
       System.Console.WriteLine("This is the eleventh statement.");
       // this is a comment
-      System.Console.WriteLine("This is the last statement.");
-      public class Print
-{
-   public static void Main()
-   {
-      // this is the first comment 
-      System.Console.WriteLine("This is the first statement.");
-      // this is a comment 
-      System.Console.WriteLine("This is the second statement.");
-      // this is a comment 
-      System.Console.WriteLine("This is the third statement.");
-      // this is a comment 
-      System.Console.WriteLine("This is the fourth statement.");
-      // this is a comment 
-      System.Console.WriteLine("This is the fifth statement.");
-       // this is a comment 
-      System.Console.WriteLine("This is the sixth statement.");
-       // this is a comment 
-      System.Console.WriteLine("This is the seventh statement.");
-      // this is a comment 
-      System.Console.WriteLine("This is the eighth statement.");
-      // this is comment
-      System.Console.WriteLine("This is the ninth statement.");
-      // this is comment
-      System.Console.WriteLine("This is the tenth statement.");
-      // this is the last comment
-      System.Console.WriteLine("This is the eleventh statement.");
-      // this is a comment
        System.Console.WriteLine("This is the last statement.");
       // end of file
-   }
-}
-
    }
 }
