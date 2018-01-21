778ac0a3b59faa94f68757ffc86d7832c1283476
Lyons Software <admin@lyonssoftware.com>
2018-01-20 23:05:58 -0600

    Create ArrayList.cs

---
+++ ArrayList.cs
@@ -0,0 +1,13 @@
+public class Addition
+{
+   public static void Main()
+   {
+      ArrayList myArrayList = new ArrayList();
+      myArrayList.Add(56);
+      myArrayList.Add("String");
+      myArrayList.Add(new Form());  
+      // end of the program
+  }
+}
+
+
