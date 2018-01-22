d7748c62263775634a4fc29496cd142199e3c60f
Lyons Software <admin@lyonssoftware.com>
2018-01-21 23:24:26 -0600

    Update ArrayList.cs

--- ArrayList.cs
+++ ArrayList.cs
@@ -2,6 +2,7 @@ public class Addition
 {
    public static void Main()
    {
+      // create instance of ArrayLIST
       ArrayList myArrayList = new ArrayList();
       myArrayList.Add(56);
       myArrayList.Add("String");
