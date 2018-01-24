c78a1d2b05e3fcbbacc32a73e2e8ceb527b389eb
Lyons Software <admin@lyonssoftware.com>
2018-01-23 23:42:02 -0600

    Update Print.cs

--- Print.cs
+++ Print.cs
@@ -2,6 +2,7 @@ public class Print
 {
    public static void Main()
    {
+      // this program prints comments 
       // this is the first comment 
       System.Console.WriteLine("This is the first statement.");
       // this is a comment 
