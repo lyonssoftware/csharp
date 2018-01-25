17cc5df6e21c5b06da66c770a646803be28a3163
Lyons Software <admin@lyonssoftware.com>
2018-01-24 23:22:03 -0600

    Update ArrayList.cs

--- ArrayList.cs
+++ ArrayList.cs
@@ -2,9 +2,11 @@ public class Addition
 {
    public static void Main()
    {
-      // create instance of ArrayLIST
+      // create instance of ArrayList
       ArrayList myArrayList = new ArrayList();
+      // add the integer 56 to the ArrayList
       myArrayList.Add(56);
+      // add string to the ArrayList
       myArrayList.Add("String");
       myArrayList.Add(new Form());  
       // end of the program
