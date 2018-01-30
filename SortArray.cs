2bdb604324fce3a0aa651762355f03e02ec475b3
Lyons Software <admin@lyonssoftware.com>
2018-01-29 22:54:20 -0600

    Update SortArray.cs

--- SortArray.cs
+++ SortArray.cs
@@ -6,7 +6,11 @@ public class SortArray
       int[] intArray = new int[5] { 8, 10, 2, 6, 3 };
       Array.Sort(intArray);
       // write array
-      foreach (int i in intArray) Console.Write(i + " ");  // output: 2 3 6 8 10  
+      foreach (int i in intArray) 
+      {
+         // output: 2 3 6 8 10 
+         Console.Write(i + " "); 
+      }
       // end of the program
   }
 }
