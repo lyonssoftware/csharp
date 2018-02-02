d677e7507de26cb1f208249a077c5f67a6b53fbc
Lyons Software <admin@lyonssoftware.com>
2018-02-01 23:03:21 -0600

    Create IfElse.cs

---
+++ IfElse.cs
@@ -0,0 +1,17 @@
+public class IfElse
+  {
+    static void Main(string[] args)
+    {
+      int a = 5, b = 5, c = 10;
+
+      if (a == b)
+        Console.WriteLine(a);
+
+      if ((a > c) || (a == b))
+        Console.WriteLine(b);
+
+      if ((a >= c) && (b <= c))
+        Console.WriteLine(c);
+    }
+  }
+}
