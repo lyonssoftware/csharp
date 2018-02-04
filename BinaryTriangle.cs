49309c53fec57189e3743952707c9c06ed191f43
Lyons Software <admin@lyonssoftware.com>
2018-02-03 22:01:31 -0600

    Create BinaryTriangle.cs

---
+++ BinaryTriangle.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+ 
+class BinaryTriangle
+    {
+        public static void Main(String[] args)
+        {
+            int p, lastInt = 0, input;
+            Console.WriteLine("Enter the Number of Rows : ");
+            input = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= input; i++)
+            {
+                for (p = 1; p <= i; p++)
+                {
+                    if (lastInt == 1)
+                    {
+                        Console.Write("0");
+                        lastInt = 0;
+                    }
+                    else if (lastInt == 0)
+                    {
+                        Console.Write("1");
+                        lastInt = 1;
+                    }
+                } Console.Write("\n");
+            }
+            Console.ReadLine();
+        }
+    }
