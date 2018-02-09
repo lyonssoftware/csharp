71ddfa9173027787f73bda83dd6f6f2887bd9e19
Lyons Software <admin@lyonssoftware.com>
2018-02-08 20:28:22 -0600

    Create Sum.cs

---
+++ Sum.cs
@@ -0,0 +1,17 @@
+class Sum
+{
+  static void Main(string[] args)
+  {
+    int num, sum = 0, r;
+    Console.WriteLine("Enter a Number : ");
+    num = int.Parse(Console.ReadLine());
+    while (num != 0)
+    {
+      r = num % 10;
+      num = num / 10;
+      sum = sum + r;
+    }
+      Console.WriteLine("Sum of Digits of the Number : "+sum);
+      Console.ReadLine();
+  }
+}
