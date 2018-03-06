e711a895a51142bdd3401cb5638a34fa4cfb71f6
Lyons Software <admin@lyonssoftware.com>
2018-03-05 22:57:03 -0600

    Create Recursion.cs

---
+++ Recursion.cs
@@ -0,0 +1,28 @@
+using System;
+class program
+{
+    public static void Main()
+{
+    int num, result;
+    pro pg = new pro();
+    Console.WriteLine("Enter the Number : ");
+    num=int.Parse(Console.ReadLine());
+    result =pg.sum(num);
+    Console.WriteLine("Sum of Digits in {0} is {1}", num, result);
+    Console.ReadLine();
+}
+}
+class pro
+{
+    public int sum(int num)
+    {
+        if (num != 0)
+        {
+            return (num % 10 + sum(num / 10));
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}
