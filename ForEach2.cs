f024e59cc232b12df4e68b94498127210693bc15
Lyons Software <admin@lyonssoftware.com>
2018-03-24 23:25:02 -0500

    Create ForEach2.cs

---
+++ ForEach2.cs
@@ -0,0 +1,11 @@
+public class ForEach2
+{
+   public static void Main()
+   {
+      var names = new List<string>() { "John", "Tom", "Peter" };
+      foreach (string name in names)
+      {
+        Console.WriteLine(name);
+      }
+  }
+}
