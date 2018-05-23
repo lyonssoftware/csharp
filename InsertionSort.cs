b1e6dbf67ff37c671c0256d41922a2ccb15bdaa8
Lyons Software <admin@lyonssoftware.com>
2018-05-22 23:50:04 -0500

    Create InsertionSort.cs

---
+++ InsertionSort.cs
@@ -0,0 +1,27 @@
+Using System;
+class InsertionSort
+{
+    private static void Main(string[] args)
+    {
+        int[] num = { 69, 56, 77, 44, 8, 0, 111, 566 };
+        for (int i = 1; i < num.Length; i++)
+        {
+            int key = num[i];
+            int j = i - 1;
+            while (j >= 0 && num[j] > key)
+            {
+                //loop
+                num[j + 1] = num[j];
+                j--;
+            }
+            num[j + 1] = key;
+        }
+
+        //display
+        foreach (var item in num)
+        {
+            Console.WriteLine(item);
+        }
+        Console.ReadKey();
+    }
+}
