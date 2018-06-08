aa931edf4b591f4f24b4590751c2dd3257401603
Lyons Software <admin@lyonssoftware.com>
2018-06-07 22:44:56 -0500

    Update Multiplication.cs

--- Multiplication.cs
+++ Multiplication.cs
@@ -5,10 +5,12 @@ using System.Text;
 
 Class Multiplication
 {
+    // multiplication 
     public static void Main()
     {
-    // demonstration of multiplying various variables 
-    // variables
+        // demostration of multiplying various variables 
+        // variables 
+        // multiply 
         int x = 3;
         int y = 8;
         // negative integer
@@ -23,7 +25,7 @@ Class Multiplication
         int f =  2;
         int g = s;
         
-        // multiply the variables
+        //multiple the variables 
         Console.WriteLine(x*y);
         Console.WriteLine(x*z);
         Console.WriteLine(x*w);
@@ -34,8 +36,8 @@ Class Multiplication
         // multiply 3 variables 
         Console.WriteLine(x*y*z);
         Console.WriteLine(a*b*s);
+        // mutiple vars 
         Console.WriteLine(i*b*p);
-        // x to the fourth degree
-        Console.WriteLine(x*x*x*x);
+        // end of the program 
         }
 }
