cb0b26cc131b03f7c62f666cc1df95d68c6bd348
Lyons Software <admin@lyonssoftware.com>
2018-06-11 22:41:41 -0500

    Create QuickSort2.cs

---
+++ QuickSort2.cs
@@ -0,0 +1,32 @@
+public static void QuickSort (int[] data, int l, int r)
+      {
+         int i, j;
+         int x;
+ 
+         i = l;
+         j = r;
+
+         x = data [(l + r) / 2]; /* find pivot item */
+         while (true) {
+            while (data[i] < x)
+               i++;
+            while (x < data[j])
+               j--;
+            if (i <= j) {
+               exchange (data, i, j);
+               i++;
+               j--;
+            }
+            if (i > j)
+               break;
+         }
+         if (l < j)
+            IntArrayQuickSort (data, l, j);
+         if (i < r)
+            IntArrayQuickSort (data, i, r);
+      }
+
+      public static void IntArrayQuickSort (int[] data)
+      {
+         IntArrayQuickSort (data, 0, data.Length - 1);
+      }
