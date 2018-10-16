8c10e6e246e0129868a26c71facc28beab58cd3a
Lyons Software <admin@lyonssoftware.com>
2018-10-15 23:58:14 -0500

    Update InsertionSort.cs

--- InsertionSort.cs
+++ InsertionSort.cs
@@ -3,26 +3,31 @@ class InsertionSort
 {
     private static void Main(string[] args)
     {
-        // create int array 
+        // array
         int[] num = { 69, 56, 77, 44, 8, 0, 111, 566 };
+        
+        // for loop
         for (int i = 1; i < num.Length; i++)
         {
-            // set the key
+            /: set ghe key 
             int key = num[i];
+            //decrement
             int j = i - 1;
+            // while loop
             while (j >= 0 && num[j] > key)
             {
                 //loop
                 num[j + 1] = num[j];
                 j--;
             }
+            // set the new key
             num[j + 1] = key;
         }
 
         //display
         foreach (var item in num)
         {
-            // print the current item 
+            // print the current item
             Console.WriteLine(item);
         }
         // display the key
