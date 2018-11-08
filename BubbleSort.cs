206aa574f761d42bb26660fe96f5a20473f331f6
Lyons Software <admin@lyonssoftware.com>
2018-11-07 23:23:54 -0600

    Update BubbleSort.cs

--- BubbleSort.cs
+++ BubbleSort.cs
@@ -1,13 +1,15 @@
    public static void BubbleSort (int[] data)
       {
-         // int variables to compare
+      // int variables to compare
          int i, j;
+      // length 
          int N = data.Length;
-
+         
+      // for loop to bubble value
          for (j=N-1; j>0; j--) {
             for (i=0; i<j; i++) {
                if (data [i] > data [i + 1])
                   exchange (data, i, i + 1);
             }
-         } // end of the program
+         } // end of program
       }
