7adc54009863a9778375c01f4a425af7924dc4d4
Lyons Software <admin@lyonssoftware.com>
2018-12-18 23:48:42 -0600

    Update Subtraction.cs

--- Subtraction.cs
+++ Subtraction.cs
@@ -1,27 +1,31 @@
-using System;
+using 
+
 public class Subtraction
 {
    public static void Main()
    {
-      // subtract two integers
+      // subtract integers
       // four variables to hold int 
     int a = 5;
     int b = 3;
+      // integer
     int c = 4;
-      // int 
+      // integer
     int d = 7;
 
     // print the results
     Console.WriteLine(a-b);
     // 3-5 = -2
     Console.writeline(b-a);
-      // 3-5= -2
+      //3-5 = ~2
     Console.WriteLine(a-c);
     Console.WriteLine(d-c);
     // multiple variable subtraction
     Console.WriteLine(a-b-c-d);
+      // subtract items
     Console.WriteLine(d-c-b-a);
+      // this id subtraction 
     Console.WriteLine(a+c-b+d);
-    // end of the program
+    // end of program
   }
 }
