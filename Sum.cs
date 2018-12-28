e20703a66d7038e358cda197132619ae1009347f
Lyons Software <admin@lyonssoftware.com>
2018-12-27 23:25:14 -0600

    Update Sum.cs

--- Sum.cs
+++ Sum.cs
@@ -1,20 +1,23 @@
+Using System;
 class Sum
 {
   static void Main(string[] args)
   {
-    // 0"program to calculate sium
     // ints to hold the number and sum
     int num, sum = 0, r;
     Console.WriteLine("Enter a Number : ");
+    // read the int
     num = int.Parse(Console.ReadLine());
+    // while loop
     while (num != 0)
     {
       r = num % 10;
       num = num / 10;
       sum = sum + r;
     }
+    // print the sum 
       Console.WriteLine("Sum of Digits of the Number : "+sum);
       Console.ReadLine();
-    // end of the program 
   }
+  // end of the program
 }
