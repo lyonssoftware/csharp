8dcf3ee6c221c9d6e6f3b81f9390aca87098a58f
Lyons Software <admin@lyonssoftware.com>
2018-12-30 23:09:16 -0600

    Update SortArray.cs

--- SortArray.cs
+++ SortArray.cs
@@ -3,12 +3,11 @@ public class SortArray
 {
    public static void Main()
    {
-      // program to sort an integer array
-      // intialize int array
+      // program to sort an integer array; intialize int array
       int[] intArray = new int[5] { 8, 10, 2, 6, 3 };
       // sort the array
       Array.Sort(intArray);
-      // use the foreach method to iterate through  the array
+      // use the foreach method to iterate through the array
       foreach (int i in intArray) 
       {
          // output: 2 3 6 8 10 
