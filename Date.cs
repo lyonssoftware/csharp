e7487170e1cb4925a0a6c21a42d1e85aeada9789
Lyons Software <admin@lyonssoftware.com>
2019-01-02 22:57:57 -0600

    Update Date.cs

--- Date.cs
+++ Date.cs
@@ -2,13 +2,15 @@ Using System;
 class Date 
 {
   public static void Main()
-    {//print string
+    {
     
     // create date time 2008-03-09 16:05:07.123
 DateTime dt = new DateTime(2008, 3, 9, 16, 5, 7, 123);
 
+    // new format
 String.Format("{0:y yy yyy yyyy}", dt);  // "8 08 008 2008"   year
 String.Format("{0:M MM MMM MMMM}", dt);  // "3 03 Mar March"  month
+    thus is dstr 
 String.Format("{0:d dd ddd dddd}", dt);  // "9 09 Sun Sunday" day
 String.Format("{0:h hh H HH}",     dt);  // "4 04 16 16"      hour 12/24
 String.Format("{0:m mm}",          dt);  // "5 05"            minute
@@ -17,5 +19,5 @@ String.Format("{0:f ff fff ffff}", dt);  // "1 12 123 1230"   sec.fraction
 String.Format("{0:F FF FFF FFFF}", dt);  // "1 12 123 123"    without zeroes
 String.Format("{0:t tt}",          dt);  // "P PM"            A.M. or P.M.
 String.Format("{0:z zz zzz}",      dt);  // "-6 -06 -06:00"   time zone
-    } // end of program
-}
+    }
+} // end of program 
