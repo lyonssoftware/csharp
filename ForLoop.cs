10dcb86f195ae1cb83636a15449fa6b590214ab1
Lyons Software <admin@lyonssoftware.com>
2019-01-03 23:39:30 -0600

    Update ForLoop.cs

--- ForLoop.cs
+++ ForLoop.cs
@@ -1,4 +1,4 @@
-
+// For Loop
 class ForLoop
 {
   int main()
@@ -6,7 +6,6 @@ class ForLoop
     // incrementing for loop
     for(int i = 0; i < 10; i++)
     {
-      // print the increment
        Console.WriteLine("The is an increment for loop.");
     }
     
@@ -16,11 +15,9 @@ class ForLoop
       Console.WriteLine("The is a decrement for loop.");
     }
     
-    // increment the for loop by 2
-    for(int i = 1; i < 10; i*2;
+    // increment by the loop for(int i = 1; i < 10; i*2;
     {
-      // print the value 
-      // code
+      // print 
        Console.WriteLine("This for loop increments by 2.");
     }
     // print the line up to 5
@@ -31,10 +28,8 @@ class ForLoop
     }
     // print the line up to 10 
     for (int k = 0; k < 10; k++)
-        // here 
-        // move 
     {
-      // print the result 
+      // print 
       Console.WriteLine("This is line " + k);
     }
   } // end of the program 
