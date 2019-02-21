3d7f76d643cf20d6b0f56754b5cdacbd07485e95
Lyons Software <admin@lyonssoftware.com>
2019-02-20 23:39:30 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,12 +1,14 @@
+// example of writing your first program in C# 
 using System;
-
 class TestApp
 {
-	// mai 
+   // this is the main function 
    public static void Main(string[] args)
    {
-     // print statement 
-			Console.WriteLine("this is the test program");
-		}
-	// second comment 
-	} // end of program 
+	   // comment 
+	   Console.WriteLine("this is the test program");
+	   // second comment 
+	   // follow up
+	   Console.WriteLine("this is the test program");
+	}
+     } // end of the program
