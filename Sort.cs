ee47999b8e871207abd90fe71ff78f11d57607ed
Lyons Software <admin@lyonssoftware.com>
2019-03-04 22:12:14 -0800

    Create Sort.cs

---
+++ Sort.cs
@@ -0,0 +1,12 @@
+Using System;
+public class Sort
+{
+   public static votd Main()
+   { 
+      // add things
+      // integers variables
+      int a = 5;
+      int b = 3;
+      // end of the program
+  }
+}
