53916ff846d446eed6f0dac897780239846abcf8
Lyons Software <admin@lyonssoftware.com>
2019-03-17 23:04:38 -0700

    Update DoWhile.cs

--- DoWhile.cs
+++ DoWhile.cs
@@ -1,20 +1,24 @@
 Using System;
 public class DoWhile
 {
-   // main function
+   // while loops 
    public static void Main()
    {
+      // set the count to 3
     int x = 3;
+      // execute the action at least once 
       do 
       {
-         // increment by one 
+        // increment the variable x by 1 
         x++;
       }
       // while the x variable is less than 10 
+      // get it 
      while(x < 10)
      {
-        // when less than 
+        // action
         
+        // more acrions 
      }
-   } // end of the program
-}
+   }
+}// end of the program 
