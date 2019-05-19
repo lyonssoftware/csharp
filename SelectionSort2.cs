92bcb6bcf03d18566846a702a7613a7cf36df35c
Lyons Software <admin@lyonssoftware.com>
2019-05-18 22:36:23 -0700

    Update SelectionSort2.cs

--- SelectionSort2.cs
+++ SelectionSort2.cs
@@ -8,7 +8,6 @@ class SelectionSort
         int array_size = 10;
         // create new array from array size
         int[] array = new int[10] { 100, 50, 20, 40, 10, 60, 80, 70, 90, 30 };
-        // print the unsorted array
         Console.WriteLine("The Array Before Selection Sort is: ");
         for (int i = 0; i < array_size; i++)
         {
@@ -16,22 +15,24 @@ class SelectionSort
             Console.WriteLine(array[i]);
         }
         int tmp, min_key;
- //for loop to iterate through the srray
+ // for loop 
         for (int j = 0; j < array_size - 1; j++)
         {
+// set the new key
             min_key = j;
- 
+ // for loop to iterate
             for (int k = j + 1; k < array_size; k++)
             {
                 // if correct 
                 if (array[k] < array[min_key])
                 {
-                    // set the min
+                    // setbthe min
                     min_key = k;
                 }
             }
  
             tmp = array[min_key];
+// set the libk
             array[min_key] = array[j];
             array[j] = tmp;
         }
