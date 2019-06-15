b51ab9a9deba21f53e03e25e99652153aea99a69
Lyons Software <admin@lyonssoftware.com>
2019-06-14 22:47:31 -0700

    Update Hello.cs

--- Hello.cs
+++ Hello.cs
@@ -2,10 +2,9 @@ public class Hello
 {
    public static void Main()
    {
+// Rony hello world 
       System.Console.WriteLine("Hello, World!");
       // second comment 
       System.Console.WriteLine("bob ");
-      
    }
 }
-bbjhbn
