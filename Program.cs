c6f62dc20b73039ed139629b8e9d171e5828eefd
Lyons Software <steve.lyons@lyonssoftware.com>
2019-07-21 22:43:32 -0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -9,5 +9,6 @@ class TestApp
 	   Console.WriteLine("this is the test program");
 	   // print the comment "This is the test program"
 	   Console.WriteLine("this is the test program");
+// last line 
 	}
   } // end of the program
