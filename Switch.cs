6a2fd392f34ca48b3c013dae439c9f2647cbfacc
Lyons Software <steve.lyons@lyonssoftware.com>
2019-08-01 23:59:07 -0700

    Update Switch.cs

--- Switch.cs
+++ Switch.cs
@@ -19,7 +19,7 @@ public class Switch
         Console.WriteLine("Two");
           // break
         break;
-          // thr final option 
+          // the last option if this fails  
       default:
           // pront 
         Console.WriteLine("Other");
