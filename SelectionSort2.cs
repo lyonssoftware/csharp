1c5927ed7a04cf8e64b18b9263d9794b87beda05
Lyons Software <steve.lyons@lyonssoftware.com>
2019-08-15 23:58:01 -0500

    Update SelectionSort2.cs

--- SelectionSort2.cs
+++ SelectionSort2.cs
@@ -8,7 +8,8 @@ class SelectionSort
         int array_size = 10;
         // create new array from array size
         int[] array = new int[10] { 100, 50, 20, 40, 10, 60, 80, 70, 90, 30 };
-        Console.WriteLine("The Array Before Selection Sort is: ");
+        // print the insortef arrsy 
+Console.WriteLine("The Array Before Selection Sort is: ");
         for (int i = 0; i < array_size; i++)
         {
             // print the array
