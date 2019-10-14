644c1428f357bacb1cbad8d075ad6913e7b17b08
Lyons Software <steve.lyons@lyonssoftware.com>
2019-10-13 23:08:28 -0500

    Update ForEach2.cs

--- ForEach2.cs
+++ ForEach2.cs
@@ -5,12 +5,12 @@ public class ForEach2
    // main function
    public static void Main()
    {
-      // variables new data strcut
+      // initialize the List 
       var names = new List<string>() { "John", "Tom", "Peter" };
       // iterate through the names 
       foreach (string name in names)
       {
-       // print the string in the array list 
+       // print the name from the array list 
         Console.WriteLine(name);
       }
   } // end of the program
