1ce53439f76afe6ac9339c9d4b0b3e3ef3e848bd
Lyons Software <steve.lyons@lyonssoftware.com>
2019-10-18 22:11:07 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,10 +5,9 @@ class TestApp
    // this is the main function 
    public static void Main(string[] args)
    {
-	  // first comment of the code
-	   Console.WriteLine("this is the test program");
-	   // print the comment "This is the test program"
-	   Console.WriteLine("this is the test program");
-// last line 
-	}
-  } // end of the program
+    // print statement to the console
+    Console.WriteLine("this is the test program");
+   // print the comment "This is the test program
+   Console.WriteLine("this is the test program");
+  }
+} // end of the program
