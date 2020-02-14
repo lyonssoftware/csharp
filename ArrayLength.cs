e39256d01ad45c05bef756a2d6ffe9a663d214a5
Lyons Software <steve.lyons@lyonssoftware.com>
2020-02-13 23:15:19 -0600

    Update ArrayLength.cs

--- ArrayLength.cs
+++ ArrayLength.cs
@@ -1,23 +1,23 @@
 Using System;
 class ArrayLength
 {
+// main 
     static void Main()
     {
-        // intitialize array
+        // intitialize the int array with five slots
         int[] arrayA = new int[5];
-        // get the length of the array
-// set the length of the array 
+        // get the length of the stay 
         int lengthA = arrayA.Length;
+        // print the array and the length
         Console.WriteLine("Length of ArrayA : {0}", +lengthA); 
         
         // set the array length 
-       
- long longLength = arrayA.LongLength;
+        long longLength = arrayA.LongLength;
         Console.WriteLine("Length of the LongLength Array  : {0}",longLength);
         int[,] twoD = new int[5, 10];
-// wri
         Console.WriteLine("The Size of 2D Array is : {0}",twoD.Length);
         // read the last line
         Console.ReadLine();
     } // end of the program
 }
+// last lines
