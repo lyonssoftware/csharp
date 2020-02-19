2b7997318161c61192647f0c0a12ad43986afa82
Lyons Software <steve.lyons@lyonssoftware.com>
2020-02-18 23:46:45 -0600

    Update IfElse.cs

--- IfElse.cs
+++ IfElse.cs
@@ -1,9 +1,11 @@
+// example program of if else statements in c#
 Using System;
 /*
   Program to test if else statements
 */
 public class IfElse
   {
+    // main function
     static void Main(string[] args)
     {
       // program to demo the if else statement
@@ -14,10 +16,10 @@ public class IfElse
       if (a == b)
         // print the a variable
         Console.WriteLine(a);
-      // or statement 
+      // if a is greater than c or a is equal to b 
       if ((a > c) || (a == b))
         Console.WriteLine(b);
-       // if a is greater than and b is less than or equal to c
+      
       if ((a >= c) && (b <= c))
         // is a is greater and b is smaller than c
         Console.WriteLine(c);
