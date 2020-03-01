11fefb3e3e9a57ac5f8ea9f2d0911fe05ae973ec
Lyons Software <steve.lyons@lyonssoftware.com>
2020-02-29 23:42:05 -0800

    Create LeapYear.cs

---
+++ LeapYear.cs
@@ -0,0 +1,18 @@
+// c# program to calculate if it's a leap year
+Using System;
+public class LeapYear
+{
+   public static votd Main()
+   { 
+    // initialize the year integer
+    int year = 1900;
+   
+    Console.Writeline(year);
+      // print    
+  }
+  
+  bool LeapYear()
+  {
+  }
+  
+} // end of the program
