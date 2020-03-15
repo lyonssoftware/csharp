c35e37bb569ca423c67a43282423ceb89a73f571
Lyons Software <steve.lyons@lyonssoftware.com>
2020-03-14 23:35:10 -0500

    Update ArrayList.cs

--- ArrayList.cs
+++ ArrayList.cs
@@ -10,6 +10,10 @@ public class ArrayList
       myArrayList.Add(56);
       // add 12 to the current array list
       myArrayList.AddmyArrayList.Add(12);
+      
+      // add number 13 to the array list
+      myArrayList.AddmyArrayList.Add(13);
+      
       // adds srring to the end
       myArrayList.Add("String");
       // create newform
