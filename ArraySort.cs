430fdf1392883a2381807e1d547eb9e32329d76d
Lyons Software <steve.lyons@lyonssoftware.com>
2020-04-07 23:11:20 -0500

    Update ArraySort.cs

--- ArraySort.cs
+++ ArraySort.cs
@@ -1,3 +1,4 @@
+// Example of array sort in C#
 using System;
 class ArraySort
 {
@@ -12,12 +13,13 @@ class ArraySort
             "aqua",
             "red"
         };
+        
         // Call Array.Sort method.
         Array.Sort(colors);
-        // iterate through the colors array
+        // iterate through the colors array and print each one on seperate lines
         foreach (string color in colors)
         {
-            // print the string 
+            // print the the colors as string
             Console.WriteLine(color);
         }
     } // end of program 
