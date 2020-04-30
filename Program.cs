903e6a962d7bc3ea98d36c5d253e7a6d354d86c8
Lyons Software <steve.lyons@lyonssoftware.com>
2020-04-29 22:43:21 -0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,12 +2,15 @@
 using System;
 class TestApp
 {
-   // this is the main function 
+   // main function 
    public static void Main(string[] args)
    {
+//new statement 
     // print statement to the console
     Console.WriteLine("this is the test program");
    // print the comment "This is the test program
+
+// print the statement "this is the test program"
    Console.WriteLine("this is the test program");
   }
 } // end of the program
