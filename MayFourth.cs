4740ec1f23d1178611849da68e3e2e6e6efc55ec
Lyons Software <steve.lyons@lyonssoftware.com>
2020-05-04 23:44:00 -0500

    Create MayFourth.cs

---
+++ MayFourth.cs
@@ -0,0 +1,14 @@
+// May the Fourth be with You!
+Using System;
+
+class MayFourth
+{
+// main 
+    static void Main()
+    {
+        // intitialize the int array with five slots
+        int[] arrayA = new int[5];
+        // get the length of the stay 
+ 
+    } // end of the program
+}
