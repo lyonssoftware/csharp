391f1eea1b028c6bf66914da589003e2ddbf5082
Lyons Software <steve.lyons@lyonssoftware.com>
2020-05-16 23:28:11 -0500

    Update ForEach2.cs

--- ForEach2.cs
+++ ForEach2.cs
@@ -7,6 +7,9 @@ public class ForEach2
    {
       // initialize the List 
       var names = new List<string>() { "John", "Tom", "Peter" };
+      
+       // initialize the second List 
+      var names2 = new List<string>() { "Laura", "Tanya", "Petra" };
       // use foreach() to iterate through the names 
       foreach (string name in names)
       {
