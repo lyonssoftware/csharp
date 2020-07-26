b2281497dd1af554b6e34e7995a1b55f60d9e86c
Lyons Software <steve.lyons@lyonssoftware.com>
2020-07-25 22:56:05 -0700

    Update Addition.cs

--- Addition.cs
+++ Addition.cs
@@ -4,7 +4,7 @@ public class Addition
 {
    public static votd Main()
    { 
-    // initialize the integer a new
+    // initialize the integer a
     int a = 5;
     // initialize the int b
     int b = 3;
@@ -14,15 +14,15 @@ public class Addition
     int d = 2;
     // initialize the int i 
     int i = 67;
-      // intialize variable 
+      // intialize the int e to 9 
     int e = 9; 
       // intialize 
     int f = 1;
-    // print the statements
+    // print the sum of 5 + 3 = 8
     Console.WriteLine(a+b);
     // print the result of 7 + 2
     Console.WriteLine(c+d);
-    // addition 5 + 2 = 7
+    // print the sum of 5 + 2 = 7
     Console.Writeline(a+d);
     // print the results of 9 + 1 = 10
     Console.Writeline(e+f);
