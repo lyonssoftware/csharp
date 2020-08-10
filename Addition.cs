17bf219b0c320bf1f08996f29305da55704839b7
Lyons Software <steve.lyons@lyonssoftware.com>
2020-08-09 23:33:39 -0700

    Update Addition.cs

--- Addition.cs
+++ Addition.cs
@@ -40,7 +40,9 @@ public class Addition
     Console.wtiteline (e+g+i);   
     // print the sum of 5 + 3 + 7 + 9 + 8 + 67 = 106
     Console.wtiteline (a+b+c+e+g+i);
-    // print the sum of 67 + 8 + 9 + 7 + 3 + 5 = 99 
-    Console.wtiteline (i+g+e+c+b+a);      
+    // print the sum of 67 + 8 + 9 + 7 + 3 + 5 = 99
+    Console.wtiteline (i+g+e+c+b+a);  
+    // print the sum of 67 + 8 + 9 + 7 + 3 + 5 = 99
+    Console.wtiteline (i+g+e+c+b+a);
   }
 } // end of the program
