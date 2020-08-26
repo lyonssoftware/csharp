b51ceddddacc5b6da58ab9edc405d731dc22446d
Lyons Software <steve.lyons@lyonssoftware.com>
2020-08-25 23:57:06 -0700

    Update ForEach.cs

--- ForEach.cs
+++ ForEach.cs
@@ -1,38 +1,30 @@
-// example of for loop in C#
-class ForLoop
+// C# program using ForEach to intertate through ArrayList
+class ForEach
 {
   // main function
-  public static void main()
-  {
-    // incrementing for loop
-    for(int i = 0; i < 10; i++)
+  static void Main(string[] args)
+  {            
+    // create instance of array list
+    ArrayList list = new ArrayList();
+    // add the first name to the array list
+    list.Add("John Doe");
+    // add a new name to array list
+    list.Add("Jane Doe");
+    // add the name "Someone Else" to the ArrayList
+    list.Add("Someone Else");
+     // add the name "Last Person" to the ArrayList
+    list.Add("Last Person");
+    // another person
+    list.Add("Another person");
+     // add the name "Last Person" to the ArrayList
+    list.Add("Last Person");
+    // foreach loop to iterate through list
+    foreach(string name in list)
     {
-      // print the statement "this is a increment for a loop
-       Console.WriteLine("This is an increment for loop.");
+      // print the name
+      Console.WriteLine(name)
+        // end 
     }
-    // For loop to print number 
-    for(int i = 10; i > 0; i--)
-    {
-      // print the numbers 1-10
-      Console.WriteLine("The is a decrement for loop.");
-      // end of print 
-    }
-    // increment by the loop for(int i = 1; i < 10; i*2;
-    {
-      // print the current number 
-       Console.WriteLine("This for loop increments by 2.");
-    }
-    // print This is line i
-    for (int j = 0; j < 5; j++)
-    {
-      // print the line number 
-      Console.WriteLine("This is line " + j);
-    }
-    // print the line up to 10 
-    for (int k = 0; k < 10; k++)
-    {
-      // print yhr line number 
-      Console.WriteLine("This is line " + k);
-    }
-  } // end of the program 
-}
+    Console.ReadLine();
+  }
+  // end of the program
