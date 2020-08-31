06419a252afc6846638ebefc5cb734b81f07866d
Lyons Software <steve.lyons@lyonssoftware.com>
2020-08-30 23:45:14 -0500

    Update Sum.cs

--- Sum.cs
+++ Sum.cs
@@ -4,16 +4,15 @@ class Sum
 {
   static void Main(string[] args)
   {
-    // initialize the variables 
+    // initialize the three variables
     int num, sum = 0, r;
     Console.WriteLine("Enter a Number : ");
-    // read the int
-// get info 
+    // read the int from the user
     num = int.Parse(Console.ReadLine());
-    // while loop
+    // while loop to iterate through the numbers
     while (num != 0)
     {
-      // set r to the remainder
+      // set r to the remainder of the number 
       r = num % 10;
       // set the new num
       num = num / 10;
@@ -22,8 +21,8 @@ class Sum
     }
     // print the sum 
       Console.WriteLine("Sum of Digits of the Number : "+sum);
-     
-// read the next line Console.ReadLine();
+    // read the next line of the program
+    Console.ReadLine();
   }
   // end of the program
 }
