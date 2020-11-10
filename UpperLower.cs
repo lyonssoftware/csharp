61abcb067b4242acd9c844545543daf882ee0e13
Lyons Software <steve.lyons@lyonssoftware.com>
2020-11-09 23:36:26 -0600

    Update UpperLower.cs

--- UpperLower.cs
+++ UpperLower.cs
@@ -6,23 +6,24 @@ class UpperLower
   {
     // create an array
     Array stringArray = Array.CreateInstance(typeof(String), 6);
-    // set the value of the first array element to "Mango"
+    // set the value of the first array to "Mango"
     stringArray.SetValue("Mango", 0);
-    // set a
+    // set array of the second item 
     stringArray.SetValue("Orange", 1);
-    // set the value of the second in 
+    // set the value
     stringArray.SetValue("Apple", 2);
-            stringArray.SetValue("Grape", 3);
+    // set the value of the 
+    stringArray.SetValue("Grape", 3);
     // set the value of the String Array
-            stringArray.SetValue("Cherry", 4);
-    // set the value of the array 
-            stringArray.SetValue("WaterMelon", 4);
+    stringArray.SetValue("Cherry", 4);
+    // set value of the array 
+    stringArray.SetValue("Black Berry", 5);
     
   // print the lower bound of the array 
             Console.WriteLine("The Lower Bound of the Array : {0}",stringArray.GetLowerBound(0).ToString());
-    // the upper bound limit of the 
+    // upper bound when it hits limit
             Console.WriteLine("The Upper Bound of the Array : {0}",stringArray.GetUpperBound(0).ToString());
     // read the last line of the arraylist
             Console.ReadLine();
         }
-    } // end of program 
+ } // end of program 
