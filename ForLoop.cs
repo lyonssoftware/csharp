8f6561f16122c49dad4c6c16d1389b8759c2831a
Lyons Software <steve.lyons@lyonssoftware.com>
2020-11-23 22:44:07 -0800

    Update ForLoop.cs

--- ForLoop.cs
+++ ForLoop.cs
@@ -1,1 +1,41 @@
-319166
+// example of for loop in C#
+class ForLoop
+{
+  // main function
+  public static void main()
+  {
+    // incrementing for loop
+    for(int i = 0; i < 10; i++)
+    {
+      // print the statement "this is a increment for a loop
+       Console.WriteLine("This is an increment for loop.");
+    }
+    // For loop to print number 
+    // from int 10 to 0 
+    for(int i = 10; i > 0; i--)
+    {
+      // print the numbers 1-10
+      Console.WriteLine("The is a decrement for loop.");
+      // end of print 
+    }
+    // increment by the loop for(int i = 1; i < 10; i*2;
+    {
+      // print the current number 
+       Console.WriteLine("This for loop increments by 2.");
+    }
+    // print This is line i
+    for (int j = 0; j < 5; j++)
+// info
+
+    {
+      // print the line number 
+      Console.WriteLine("This is line " + j);
+    }
+    // print the line up to 10 
+    for (int k = 0; k < 10; k++)
+    {
+      // print yhr line number 
+      Console.WriteLine("This is line " + k);
+    }
+  } // end of the program 
+}
