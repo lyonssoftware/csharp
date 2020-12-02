b5505d51ede1dd064a6718693578a8fb6a1a26a0
Lyons Software <steve.lyons@lyonssoftware.com>
2020-12-01 23:47:37 -0800

    Update SortArray.cs

--- SortArray.cs
+++ SortArray.cs
@@ -7,19 +7,12 @@ public class SortArray
    {
       // program to sort an integer array; intialize int array
       int[] intArray = new int[5] { 8, 10, 2, 6, 3 };
-      // make a second array // c# program to sort an int array
-Using System;
-public class SortArray
-{
-// the main program to sort array
-   public static void Main()
-   {
-      // program to sort an integer array; intialize int array
-      int[] intArray = new int[5] { 8, 10, 2, 6, 3 };
       // make a second array 
-      int[] intArray2 = new int[6] { 8, 10, 2, 6, 3, 1 };
+      int[] intArray2 = new int[6] { 6, 9, 2, 6, 5, 10 };
       
-      int[] intArray3 = new int[6] { 18, 10, 15, 6, 8, 1 };
+      int[] intArray3 = new int[6] { 18, 10, 1, 6, 8, 11 };
+      
+      int[] intArray4 = new int[6] { 1, 8, 15, 6, 8, 1 };
       // sort the integer array object intArray
       Array.Sort(intArray); 
       // use the foreach method to iterate through the array object intArray
@@ -32,17 +25,6 @@ public class SortArray
   }
 }
 
-// x
-      int[] intArray2 = new int[6] { 8, 10, 2, 6, 3, 1 };
-      // sort the integer array object intArray
-      Array.Sort(intArray); 
-      // use the foreach method to iterate through the array object intArray
-      foreach (int i in intArray) 
-      {
-// check it 
-         // print the output of the array is: 2 3 6 8 10
-         Console.Write(i + " "); 
-      }
       // end of the program
   }
 }
