07b8568d0801aba05c2b066a36e5f2bc9255d428
Lyons Software <steve.lyons@lyonssoftware.com>
2021-01-26 23:32:05 -0600

    Update Sort.cs

--- Sort.cs
+++ Sort.cs
@@ -9,6 +9,8 @@ public class Sort
       int a = 5;
       // initialize the variable b
       int b = 3;
+      
+      int c = 8;
 
       // create an int array with 5 
       Int array1 = Int{3,7, 1, 15,9};
