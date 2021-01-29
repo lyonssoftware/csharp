f59c769fd82d42b089484e0d43fb274076b19f05
Lyons Software <steve.lyons@lyonssoftware.com>
2021-01-28 23:35:33 -0600

    Update WhileLoop.cs

--- WhileLoop.cs
+++ WhileLoop.cs
@@ -8,6 +8,7 @@ public class WhileLoop
       // int to hold the counter
       int i = 5;
       int f = 0;
+      int s = 0;
       // while the count is larger than 0
       while(i > 0)
       { 
