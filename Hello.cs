a7e4c6a446b34f03714b251ebd011f37b85f26ba
Lyons Software <steve.lyons@lyonssoftware.com>
2021-01-29 23:50:36 -0600

    Update Hello.cs

--- Hello.cs
+++ Hello.cs
@@ -9,5 +9,7 @@ public class HelloWord
       System.Console.WriteLine("Hello, World!");
       // print My name is Bob
       System.Console.WriteLine("My name is Bob.");
+         // print My name is Chris
+      System.Console.WriteLine("My name is Chris.");
    } // end of the program
 }
