8f7c6732124c749cab2f2aca8cdd7347874d10d1
Lyons Software <steve.lyons@lyonssoftware.com>
2021-04-08 23:53:06 -0500

    Update LowerUpper.cs

--- LowerUpper.cs
+++ LowerUpper.cs
@@ -6,7 +6,7 @@ class LowerUpper
   static void Main(string[] args)
   {
     // create a new Array object and set the size to 6 
-    Array stringArray = Array.CreateInstance(typeof(String), 12);
+    Array stringArray = Array.CreateInstance(typeof(String), 13);
     // insert the string "Mango" into the first index of array
     stringArray.SetValue("Mango", 0);
     // insert the string "Orange" into the second index of array
@@ -32,6 +32,9 @@ class LowerUpper
 stringArray.SetValue("Apricot", 10);
     // set the value
     stringArray.SetValue("Dragonfruit", 11);
+    
+        // set the value
+    stringArray.SetValue("Black Cherry", 12);
 
     //print "the lower" 
             Console.WriteLine("The Lower Bound of the Array : {0}",stringArray.GetLowerBound(0).ToString());
