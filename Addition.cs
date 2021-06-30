dea1f83fa140169e0438ee5fcd63e97f74ca33ac
Lyons Software <steve.lyons@lyonssoftware.com>
2021-06-29 23:40:44 -0500

    Update Addition.cs

--- Addition.cs
+++ Addition.cs
@@ -7,15 +7,6 @@ public class Addition
     // initialize the integer a
     int a = 5;
     // initialize the int b// program to add integers
-Using System;// program to add integers
-Using System;
-public class Addition
-{
-   public static votd Main()
-   { 
-    // initialize the integer a
-    int a = 5;
-    // initialize the int b// program to add integers
 Using System;
 public class Addition
 {
@@ -98,130 +89,7 @@ int l = 38;
       
       int ee = 33; 
       
-      int rr = 689;
-      
-      int tt = 58;
-      
-      int u = 52;
-      
-      int ff = 5;
-      
-      int uu = 67;
-      
-      int xx = 31;
-      
-      int yy = 29;
-      
-      int zz = 15;
-
-    // initialize the int q to 20
-    int q = 20;
-Int h = 24;
-    // print the sum of 5 + 3 = 8
-    Console.WriteLine(a+b);
-    // print the sum of 7 + 2 = 9
-    Console.WriteLine(c+d);
-    // print the sum of 5 + 2 = 7
-    Console.Writeline(a+d);
-    // print the sum of 9 + 1 = 10
-    Console.Writeline(e+f);
-    // print the sum of 5 + 1 = 6
-    Console.Writeline(a+c);
-    // print the sum of 3 + 2 = 5
-    Console.Writeline(b+d);
-     // print the sum of 3 + 2 = 5
-    Console.wtiteline (a+e);
-    // print the sum of 5 + 9 = 14
-    Console.wtiteline (a+b+c);
-    // print the sum of 9 + 8 + 67 = 84
-    Console.wtiteline (e+g+i);   
-    // print the sum of 5 + 3 + 7 + 9 + 8 + 67 = 106
-    Console.wtiteline (a+b+c+e+g+i);
-    // print the sum of 67 + 8 + 9 + 7 + 3 + 5 = 99
-    Console.wtiteline (i+g+e+c+b+a);  
-    // print the sum of 67 + 8 + 9 + 7 + 3 + 5 = 99
-    Console.wtiteline (i+g+e+c+b+a);
-  }
-} // end of the program
-
-public class Addition
-{
-   public static votd Main()
-   { 
-    // initialize the integer a
-    int a = 5;
-    // initialize the int b
-    int b = 3;
-     // initialize the int c
-    int c = 7;
-     // initialize the int d
-    int d = 2;
-    // initialize the int i 
-    int i = 67;
-      // intialize the int e to 9 
-    int e = 9; 
-    // intialize the int f to 1
-    int f = 1;
-    // intialize the int g to 8
-    int g = 8;
-    // initialize the int h to 10
-    int h = 10;
-      // iniy 
-      int k = 32;
-      
-      int v = 98;
-      
-      int l = 19;
-
-      int vv = 932;
-int cc =69;
-      
-      int y = 15;
-      
-      int kk = 324;
-      
-      int rr = 295;
-      
-      int oo = 19;
-      
-      int tt = 39;
-      
-      int yyu = 24;
-      
-      int oo = 4;
-
-int s = 45;
-      int ss = 457;
-      
-      int oo = 0;
-      int o = 65;
-      int mm = 92;
-      
-      int ot = 78;
-      
-      int lk = 64;
-      
-      int ytl = 78;
-      
-      int oa = 29;
-      
-      int hl = 98;
-      
-      int nn = 29;
-      
-      int m = k;
-
-int l = 38;
-      
-      int ll = 89;
-      
-      int jj = 28;
-      
-      int pp = 989;
-      
-      int qq = 48;
-      
-      int ee = 33; 
+      int ww = 38;
       
       int rr = 689;
       
