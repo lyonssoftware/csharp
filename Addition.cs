3cedfde401bdd02acb53041464da35619599ba55
Lyons Software <steve.lyons@lyonssoftware.com>
2022-04-03 23:41:26 -0500

    Update Addition.cs

--- Addition.cs
+++ Addition.cs
@@ -1,367 +1,108 @@
-// program to add integers
+// c#  of using arrays
 Using System;
-public class Addition
+public class Array
 {
    public static void Main()
-   { 
-    // initialize the integer a
-    int a = 15;
-    // initialize the integer aa
-    int aa = 30;
-    // initialize the integer b
-    int b = 20;
-    // initialize the int b
-    int bb = 40;
-    // initialize the int c
-    int c = 17;
-    // initialize the int s
-    int s = 67;
-    // initialize the int cc
-    int cc = 14;
-    // initialize the int d
-    int d = 2;
-    // initialize the int dd
-    int dd = 4;
-    // initialize the int i 
-    int i = 67;
-    // initialize the int kj
-    int kj = 18;
-    // initialize the int li
-    int ii = 134;
-    // intialize the int e to 9 
-    int e = 9; 
-    // intialize the int j to 23
-    int j = 23;
-    // intialize the int jj to 83 
-    int jj = 83; 
-    // intialize the int ee to 10
-    int ee = 18;
-    // intialize the int f to 1
-    int f = 1;
-    // intialize the int ff to 2
-    int ff = 2;
-    // intialize the int g to 8
-    int g = 8;
-    // intialize the int gg to 16
-    int gg = 16;
-    // initialize the int h to 10
-    int h = 10;
-    // initialize the int hh to 10
-    int hh = 20;
-    // initialize the int k to 32
-    int k = 32;
-    // initialize the int kk to 67
-    int kk = 67;
-    // initialize the int vv to 45 
-    int vv = 45;
-    // initialize the int v to 98
-    int v = 98;
-    // initialize the int l to 19
-    int l = 19;
-    // initialize the int ll to 98
-    int ll = 98;
-    // initialize the int lll to 39
-    int lll = 39;
-    // initialize the int vv to 932
-    int vv = 932;
-    // initialize the int cc to 69
-    int cc = 69;
-    // initialize the int y to 15
-    int y = 15;
-    // initialize the int yy to 30 
-    int yy = 30;
-    // initialize the int kk to 324
-    int kk = 324;
-    // initialize the int rr to 30 
-    int rr = 30;
-    // initialize the int oo to 19
-    int oo = 19;
-    // initialize the int tt to 39
-    int tt = 39;
-    // initialize the int yyu to 24  
-    int yyu = 24;
-    // initialize the int df to 108
-   int df = 108;
-   // initialize the int yyu to 24 
-   int oo = 4;
-   // initialize the int s to 45
-   int s = 45;
-   // initialize the int ss to 457
-   int ss = 457;
-   // initialize the int oo to 0
-   int oo = 0;
-   // initialize the int o to 65
-   int o = 65;
-   // initialize the int mm to 92
-   int mm = 92;
-   // initialize the int ot to 78
-   int ot = 78;
-   // initialize the int lk to 64
-   int lk = 64;
-   // initialize the int jl to 29 
-   int jl = 29;
-   // initialize the int ytl to 78
-   int ytl = 78;
-   // initialize the int oa to 29
-   int oa = 29;
-   // initialize the int yo to 68
-   int yo = 68;
-   // initialize the int d to 56
-   int d = 56;
-   // initialize the int hl to 98
-   int hl = 98;
-   // initialize the int nn to 29
-   int nn = 29;
-   // initialize the int m to the int k
-   int m = k;
-   // initialize the int l to 38
-   int l = 38;
-   // initialize the int ll to 89   
-   int ll = 89;
-   // initialize the int jj to 28      
-   int jj = 28;
-   // initialize the int jjj to 56      
-   int jjj = 56;
-   // initialize the int gg to 49   
-   int gg = 49;
-   // initialize the int pp to 989 
-   int pp = 989;
-   // initialize the int pp to 989 
-   int qq = 48;
-   // initialize the int ee to 33    
-   int ee = 33; 
-   // initialize the int kc to 18    
-   int kc = 18;
-   // initialize the int ww to 38   
-   int ww = 38;
-   // initialize the int h to 29 
-   int h = 29;   
-   // initialize the int rr to 689      
-   int rr = 689;
-   // initialize the int tt to 58 
-   int tt = 58;
-   // initialize the int u to 52 
-   int u = 52;
-   // initialize the int uu to 104 
-   int uu = 104;
-   // initialize the int df to 5
-   int ff = 5;
-   // initialize the int hg to 89   
-   int hg = 89;
-   // initialize the int uu to 67   
-   int uu = 67;
-   // initialize the int xx to 31
-   int xx = 31;
-   // initialize the int yy to 29 
-   int yy = 29;
-   // initialize the int zz to 15 
-   int zz = 15;
-   // initialize the int q to 20
-   int q = 20;
-   // initialize the int qq to 29 
-   int qq = 40;
-   // initialize the int h to 24
-   int h = 24;
-   // initialize the int p to 45
-   int p = 45;
-   // initialize the int hh to 48
-   int hh = 48; 
-   // initialize the int dd to 32
-   int dd = 32;
-    // initialize the int bb to 25
-    int bb = 25;  
-    // initialize the int zzz to 25
-    int zzz = 25;
-    // initialize the int yyy to 50
-    int yyy = 50;
-    // initialize the int uo to 20
-    int uo = 20;
-    // initialize the int pn to 45
-    int pn = 45;
-    //dsas initialize the int ui to 50
-    int ui = 50;
-    // initialize the int ui to 50
-    int ui = 50;
-    // initialize the int op to 28
-    int op = 28;
-    // initialize the int dk to 209
-    int dk = 209;
-    // initialize the int mn to 76
-    int mn = 76;
-    // initialize the int df to 89
-    int df = 89;
-    // initialize the int od to 32  
-    int od = 32;
-    // initialize the int fff to 56
-    int fff = 56;
-    // initialize the int ccc to 35
-    int ccc = 35;
-    // initialize the int ddd to 86
-    int ddd = 86;
-    // initialize the int eee to 29
-    int eee = 29;
-    // initialize the int qqq to 32
-    int qqq = 32;
-    // initialize the int yyy to 27
-    int yyy = 27;
-    // initialize the int zzz to 578
-    int zzz = 578;
-    // initialize the int zzz to 578
-    int rrr = 34;
-    // initialize the int ggg to 28
-    int ggg = 28;
-    // initialize the int vvv to 87
-    int vvv = 87;
-    // initialize the int sss to 67
-    int sss = 67;
-    // initialize the int aaa to 67
-    int aaa = 38;
-    // initialize the int www to 58
-    int www = 58;
-    // initialize the int lll to 38
-    int = lll = 38;
-    // initialize the int rrr to 56
-    int rrr= 56;
-    // initialize the int ggg to 100
-    int ggg = 100;
-    // initialize the int aaa to 38
-    int aaa = 38;
-    // initialize the int yy to 90  
-    int yy = 90;
-    // initialize the int ttt to 48
-    int ttt = 48;
-    // initialize the int nnn to 78
-    int nnn = 78;
-    // initialize the int uuu to 5
-    int uuu = 5;
-    // initialize the int mmm to 98
-    int mmm = 98;
-    // initialize the int eee to 55    
-    int eee = 55;
-    // initialize the int hhh to 28
-    int hhh = 28;
-    // initialize the int qqq to 78
-    int qqq = 78;
-    // initialize the int ttt to 532
-    int ttt = 532;
-    // initialize the int nnn to 32
-    int nnn = 32;
-    // initialize the int mmm to 89
-    int mmm = 89;
-    // initialize the int ccc to 87
-    int ccc = 87;
-    // initialize the int tnt to 800
-    int tnt = 800;
-    // initialize the int PHP to 57
-    int PHP = 57;
-    // initialize the int ppp to 45
-    int ppp = 45;
-    // initialize the int fff to 39
-    int fff = 39;
-    // initialize the int krk to 32
-    int krk = 32;
-    // initialize the int wow to 97
-    int wow = 92;
-    // initialize the int sns to 23
-    int sns = 23;
-    // initialize the int iou to 50
-    int iou = 50;
-    // initialize the int iou to 50
-    int iou = 50;
-    // initialize the int you to 30
-    int iou = 30;
-    // initialize the int we to 14
-    int we = 14;
-    // initialize the int io to 67
-    int io = 67;
-    // initialize the int zion to 75
-    int zion = 75;
-    // initialize the int lvl to 69
-    int lvl = 69;
-    // initialize the int you to 100
-    int you = 100;
-    // initialize the int opo to 25
-    int opo = 25;
-    // initialize the int iou to 50
-    int iou = 50;
-    // initialize the int iop to 30
-    int iop = 30;  
-    // initialize the int nmo to 63
-    int nmo = 63;  
-   // initialize the int nmo to 15
-   int imo = 15;    
+   {
+     // create an int array of 31 indexes 
+     int[] array = new int[25];
       
-    // print the sum of 5 + 3 = 8
-    Console.WriteLine(a+b);
-    // print the sum of 7 + 2 = 9
-    Console.WriteLine(c+d);
-    // print the sum of 5 + 2 = 7
-    Console.Writeline(a+d);
-    // print the sum of 9 + 1 = 10
-    Console.Writeline(e+f);
-    // print the sum of 5 + 1 = 6
-    Console.Writeline(a+c);
-    // print the sum of 3 + 2 = 5
-    Console.Writeline(b+d);
-     // print the sum of 3 + 2 = 5
-    Console.WriteLine(a+e);
-    // print the sum of 5 + 9 = 14
-    Console.WriteLine(a+b+c);
-    // print the sum of 9 + 8 + 67 = 84
-    Console.WriteLine(e+g+i);   
-    // print the sum of 5 + 3 + 7 + 9 + 8 + 67 = 106
-    Console.WriteLine(a+b+c+e+g+i);
-    // print the sum of 67 + 8 + 9 + 7 + 3 + 5 = 99
-    Console.WriteLine(i+g+e+c+b+a);  
-    // print the sum of 67 + 8 + 9 + 7 + 3 + 5 = 99
-    Console.WriteLine(i+g+e+c+b+a);
-    // print the sum of 15 + 30 + 60 + 90 = 195
-    Console.WriteLine(aa+bb+cc+dd);
-    // print the sum of 
-    Console.WriteLine(bb+ee+ii+dd);
-    // print the sum of 
-    Console.WriteLine(ee+aa+gg+dd);  
-    // print the sum of 
-    Console.WriteLine(bb+k+ii+l);  
-    // print the sum of 
-    Console.WriteLine(cc+kk+dd); 
-    // print the sum of 
-    Console.WriteLine(aa+kk+dd+ii+bb); 
-    // print the sum of 
-    Console.WriteLine(k+dd); 
-    // print the sum of 
-    Console.WriteLine(aa+kk+dd+ii+bb); 
-    // print the sum of   
-    Console.WriteLine(cc+kk+dd); 
-    // print the sum of 
-    Console.WriteLine(aa+kk+dd);
-    // print the sum of 
-    Console.WriteLine(aa+kk+dd+ii+bb); 
-    // print the sum of 
-    Console.WriteLine(cc+kk+dd); 
-    // print the sum of 
-    Console.WriteLine(aa+ii+bb); 
-    // print the sum of   
-    Console.WriteLine(cc+kk+jj); 
-    // print the sum of 
-    Console.WriteLine(k+dd); 
-    // print the sum of 
-    Console.WriteLine(aa+kk+dd+ii+bb); 
-    // print the sum of   
-    Console.WriteLine(cc+kk+dd); 
-    // print the sum of 
-    Console.WriteLine(aa+kk+dd);
-    // print the sum of 
-    Console.WriteLine(cc+kk+dd); 
-    // print the sum of 
-    Console.WriteLine(aa+ii+bb); 
-    // print the sum of   
-    Console.WriteLine(cc+kk+jj); 
-    // print the sum of 
-    Console.WriteLine(aa+bb); 
-    // print the sum of   
-    Console.WriteLine(dd); 
-    // print the sum of 
-    Console.WriteLine(aa+bb+cc+dd); 
-  }
-} // end of the program
+      // create a double array
+      double[] doubleArray = new double[10];
+      
+      // set the first array entry to 1
+      array[0] = 1;
+      // add to the array
+      array[1] = 2;
+      // add to the array
+      array[2] = 3;
+      // add to the int array
+      array[3] = 4;
+      // add to the int array 
+      array[4] = 5;
+      // add to the int array 
+      array[5] = 6;
+      // add to the int array 
+      array[6] = 7; 
+      // add to the int array 
+      array[7] = 8; 
+      // add to the int array 
+      array[8] = 9;
+      // set the value of the index 
+      array[9] = 10;
+      // add to the int array 
+      array[10] = 11;
+      // set the value of the index 
+      array[11] = 12;
+      // set the value of the index 
+      array[12] = 13;
+      // set the value of the index 
+      array[13] = 14;
+      // set the value of the index 
+      array[14] = 15;
+      // set the value of the index 
+      array[15] = 16;
+      // set the value of the index 
+      array[16] = 17;
+      // set the value of the index 
+      array[17] = 18;
+      // set the value of the index 
+      array[18] = 19;
+       // set the value of the index 
+      array[19] = 20;
+      // set int value
+      array[20] = 21;
+       // set the value of the index 
+      array[21] = 22;
+       // set the value of the index 
+      array[22] = 23;
+      // add to the array
+      array[23] = 45;
+       // set the value of the index 
+      array[24] = 25;
+      // print the first element 
+      Console.WriteLine(array[0]);
+      // print the second element 
+      Console.WriteLine(array[1]);
+      // print the third element 
+      Console.WriteLine(array[2]);
+      // print the fourth element 
+      Console.WriteLine(array[3]);
+      Console.WriteLine(array[4]);
+      Console.WriteLine(array[5]);
+      Console.WriteLine(array[6]);
+      Console.WriteLine(array[7]);
+      Console.WriteLine(array[8]);
+      Console.WriteLine(array[9]);
+      Console.WriteLine(array[10]);
+      Console.WriteLine(array[11]);
+      Console.WriteLine(array[12]);
+      Console.WriteLine(array[13]);
+      Console.WriteLine(array[14]);
+      Console.WriteLine(array[15]);
+      Console.WriteLine(array[16]);
+// print the result 
+      Console.WriteLine(array[17]);
+      Console.WriteLine(array[18]);
+      Console.WriteLine(array[19]);
+      Console.WriteLine(array[20]);
+      Console.WriteLine(array[21]);
+      Console.WriteLine(array[22]);
+      Console.WriteLine(array[23]);
+      Console.WriteLine(array[24]);
+      Console.WriteLine(array[25]);
+      Console.WriteLine(arrray[26]);
+      Console.WriteLine(arrray[27]);
+      Console.WriteLine(arrray[28]);
+      Console.WriteLine(arrray[29]);
+      Console.WriteLine(array[30]);
+      Console.WriteLine(array[31]);
+      Console.Writelibe(array[32]);
+      Console.WriteLine(array[33]);
+      Console.WriteLine(array[34]);
+      Console.WriteLine(array[35]);
+      Console.WriteLine(array[36]);
+      CONSOLE.writeline(array[37]);
+   }
+   // the end of program
+}
