176afa682450381a10a762725a360c0e9c3974bd
Lyons Software <steve.lyons@lyonssoftware.com>
2022-08-31 22:06:21 -0700

    Update Array.cs

--- Array.cs
+++ Array.cs
@@ -210,7 +210,337 @@ public class Array
       array[95] = 92;
       // set the value of the index
       array[96] = 93;
+      
+      // c#  of using arrays
+Using System;
+public class Array
+{
+   public static void Main()
+   {
+     // create an int array of 31 indexes 
+     int[] array = new int[25];
+      
+      // create a double array
+      double[] doubleArray = new double[10];
+    
+      // set the first array entry to 1
+      array[0] = 16;
+      // add to the array
+      array[1] = 20;
+      // add to the array
+      array[2] = 23;
+      // add to the int array
+      array[3] = 45;
+      // add to the int array 
+      array[4] = 15;
+      // add to the int array 
+      array[5] = 16;
+      // add to the int array 
+      array[6] = 17; 
+      // add to the int array 
+      array[7] = 18; 
+      // add to the int array 
+      array[8] = 19;
+      // set the value of the index 
+      array[9] = 10;
+      // add to the int array 
+      array[10] = 11;
+      // set the value of the index 
+      array[11] = 12;
+      // set the value of the index 
+      array[12] = 13;
+      // set the value of the index 
+      array[13] = 14;
+      // set the value of the index 
+      array[14] = 15;
+      // set the value of the index 
+      array[15] = 16;
+      // set the value of the index 
+      array[16] = 17;
+      // set the value of the index 
+      array[17] = 18;
+      // set the value of the index 
+      array[18] = 19;
+      // set the value of the index 
+      array[19] = 20;
+      // set the value of the index
+      array[20] = 21;
+      // set the value of the index 
+      array[21] = 22;
+       // set the value of the index
+      array[22] = 22;
+      // set the value of the index
+      array[23] = 23;
+      // set the value of the index
+      array[24] = 24;
+      // set the value of the index
+      array[25] = 25;
+      // set the value of the index
+      array[26] = 26;
+      // set the value of the index
+      array[27] = 27;
+      // set the value of the index
+      array[28] = 28;
+      // set the value of the index
+      array[29] = 29;
+      // set the value of the index
+      array[30] = 30;
+      // set the value of the index
+      array[31] = 31;
+      // set the value of the index
+      array[32] = 32;
+      // set the value of the index
+      array[33] = 33;
+      // set the value of the index
+      array[34] = 34;
+      // set the value of the index
+      array[35] = 35;
+      // set the value of the index
+      array[36] = 36;
+      // set the value of the index
+      array[37] = 37;
+      // set the value of the index
+      array[38] = 38;
+      // set the value of the index
+      array[39] = 39;
+      // set the value of the index
+      array[40] = 40;
+      // set the value of the index
+      array[41] = 41;
+      // set the value of the index
+      array[42] = 42;
+      // set the value of the index
+      array[43] = 43;
+      // set the value of the index
+      array[44] = 44;
+      // set the value of the index
+      array[45] = 45;
+      // set the value of the index
+      array[46] = 46;
+      // set the value of the index
+      array[47] = 47;
+      // set the value of the index
+      array[48] = 48;
+      // set the value of the index
+      array[49] = 49;
+      // set the value of the index
+      array[50] = 50;
+       // set the value of the index
+      array[51] = 51;
+      // set the value of the index
+      array[52] = 52;
+      // set the value of the index
+      array[53] = 53;
+      // set the value of the index
+      array[54] = 54;
+      // set the value of the index
+      array[55] = 55;
+      // set the value of the index
+      array[56] = 56;
+      // set the value of the index
+      array[57] = 57;
+      // set the value of the index
+      array[58] = 58;
+      // set the value of the index
+      array[59] = 59;
+      // set the value of the index
+      array[60] = 60;
+      // set the value of the index
+      array[61] = 61;
+      // set the value of the index
+      array[62] = 62;
+      // set the value of the index
+      array[63] = 63;
+      // set the value of the index
+      array[64] = 64;
+      // set the value of the index
+      array[65] = 65;
+      // set the value of the index
+      array[66] = 66;
+      // set the value of the index
+      array[67] = 67;
+      // set the value of the index
+      array[68] = 68;
+      // set the value of the index
+      array[69] = 69;
+      // set the value of the index
+      array[69] = 69;
+      // set the value of the index
+      array[70] = 70;
+      // set the value of the index
+      array[71] = 71;
+      // set the value of the index
+      array[72] = 72;
+      // set the value of the index
+      array[73] = 73;
+      // set the value of the index
+      array[74] = 73;
+      // set the value of the index
+      array[75] = 74;
+      // set the value of the index
+      array[76] = 75;
+      // set the value of the index
+      array[77] = 76;
+      // set the value of the index
+      array[78] = 77;
+      // set the value of the index
+      array[79] = 77;
+      // set the value of the index
+      array[80] = 78;
+      // set the value of the index
+      array[81] = 79;
+      // set the value of the index
+      array[82] = 80;
+      // set the value of the index
+      array[83] = 81;
+      // set the value of the index
+      array[84] = 82;
+      // set the value of the index
+      array[85] = 83;
+      // set the value of the index
+      array[86] = 84;
+      // set the value of the index
+      array[87] = 84;
+      // set the value of the index
+      array[88] = 84;
+      // set the value of the index
+      array[89] = 84;
+      // set the value of the index
+      array[90] = 85;
+      // set the value of the index
+      array[90] = 85;
+      // set the value of the index
+      array[91] = 85;
+      // set the value of the index
+      array[92] = 85;
+      // set the value of the index
+      array[93] = 90;
+      // set the value of the index
+      array[93] = 90;
+      // set the value of the index
+      array[94] = 91;
+      // set the value of the index
+      array[95] = 92;
+      // set the value of the index
+      array[96] = 93;
+      
+      array[97] = 94;
 
+      // print the first element 
+      Console.WriteLine(array[0]);
+      // print the second element 
+      Console.WriteLine(array[1]);
+      // print the third element 
+      Console.WriteLine(array[2]);
+      // print the fourth element 
+      Console.WriteLine(array[3]);
+      // print the fifth element 
+      Console.WriteLine(array[4]);
+      // print the sixth element 
+      Console.WriteLine(array[5]);
+      // print the seventh element 
+      Console.WriteLine(array[6]);
+      // print the eighth element 
+      Console.WriteLine(array[7]);
+      // print the ninth element 
+      Console.WriteLine(array[8]);
+      // print the tenth element 
+      Console.WriteLine(array[9]);
+      // print the eleventh element 
+      Console.WriteLine(array[10]);
+      // print the twelth element 
+      Console.WriteLine(array[11]);
+      // print the thirteenth element 
+      Console.WriteLine(array[12]);
+      // print the fourteenth element 
+      Console.WriteLine(array[13]);
+      // print the fifteenth element 
+      Console.WriteLine(array[14]);
+      // print the sixteenth element
+      Console.WriteLine(array[15]);
+       // print the seventeenth element
+      Console.WriteLine(array[16]);
+      // print the eighteenth element
+      Console.WriteLine(array[17]);
+       // print the nineteenth element
+      Console.WriteLine(array[18]);
+      // print the twenieth element
+      Console.WriteLine(array[19]);
+      // print the twenty-first element
+      Console.WriteLine(array[20]);
+      // print the twenty-second element
+      Console.WriteLine(array[21]);
+      // print the twenty-third element
+      Console.WriteLine(array[22]);
+      // print the twenty-fourth element
+      Console.WriteLine(array[23]);
+      // print the twenty-fifth element
+      Console.WriteLine(array[24]);
+      // print the twenty-sixth element
+      Console.WriteLine(array[25]);
+      // print the twenty-seventh element
+      Console.WriteLine(arrray[26]);
+      // print the twenty-eighth element
+      Console.WriteLine(arrray[27]);
+      // print the twenty-ninth element
+      Console.WriteLine(arrray[28]);
+      // print the thirtieth element
+      Console.WriteLine(arrray[29]);
+      // print the thirty-first
+      Console.WriteLine(array[30]);
+      // print the thirty-second
+      Console.WriteLine(array[31]);
+      // print the thirty-third
+      Console.Writelibe(array[32]);
+      // print the thirty-fourth
+      Console.WriteLine(array[33]);
+      // print the thirty-fifth
+      Console.WriteLine(array[34]);
+      // print the thirty-sixth
+      Console.WriteLine(array[35]);
+      // print the thirty-seventh
+      Console.WriteLine(array[36]);
+      // print the thirty-eighth
+      Console.WriteLine(array[37]);
+      // print the thirty-ninth
+      Console.WriteLine(array[38]);
+      // print the fourtieth
+      Console.WriteLine(array[39]);
+      // print the forty-first
+      Console.WriteLine(array[40]);
+      // print the forty-second
+      Console.WriteLine(array[41]);
+      // print the forty-third
+      Console.WriteLine(array[42]);
+      // print the forty-fourth
+      Console.WriteLine(array[43]);
+      // print the forty-fifth
+      Console.WriteLine(array[44]);
+      // print the forty-sixth
+      Console.WriteLine(array[45]);
+      // print the forty-seventh
+      Console.WriteLine(array[46]);
+      // print the forty-eighth
+      Console.WriteLine(array[47]);
+      // print the forty-ninth
+      Console.WriteLine(array[48]);
+       // print the fifthieth
+      Console.WriteLine(array[49]);
+      // print the fifty-first
+      Console.WriteLine(array[50]);
+      // print the fifty-second
+      Console.WriteLine(array[51]);
+      // print the fifty-third
+      Console.WriteLine(array[52]);
+      // print the fifty-fourth
+      Console.WriteLine(array[53]);
+   }
+   // the end of program
+}
+
+
+      
+      
       // print the first element 
       Console.WriteLine(array[0]);
       // print the second element 
