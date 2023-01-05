d242bd162ac44c1547c7eb855ade5e449b178284
Lyons Software <steve.lyons@lyonssoftware.com>
2023-01-04 23:57:01 -0600

    Update Array.cs

--- Array.cs
+++ Array.cs
@@ -348,6 +348,9 @@ public class Array
       // set the value of the index 
       array[166] = 166;
 
+ // set the value of the index 
+      array[167] = 166;
+
       // print the first element 
       Console.WriteLine(array[0]);
       // print the second element 
