c923847da83f258557cfe1da10d109092735c07f
Lyons Software <steve.lyons@lyonssoftware.com>
2023-04-30 23:38:10 -0700

    Update Array.cs

--- Array.cs
+++ Array.cs
@@ -423,8 +423,551 @@ public class Array
       array[204] = 204;
       // set the value of the index 
       array[205] = 205;
+  // c#  of using arrays
+Using System;
+public class Array
+{
+   public static void Main()
+   {
+     // create an int array of 200 indexes 
+     int[] array = new int[200];
+    
+      // set the first array entry to 1
+      array[0] = 0;
+      // add to the array
+      array[1] = 1;
+      // add to the array
+      array[2] = 2;
+      // add to the int array
+      array[3] = 3;
+      // add to the int array 
+      array[4] = 4;
+      // add to the int array 
+      array[5] = 5;
+      // fadd to the int array 
+      array[6] = 6; 
+      // add to the int array 
+      array[7] = 7; 
+      // add to the int array 
+      array[8] = 8;
       // set the value of the index 
+      array[9] = 9;
+      // add to the int array 
+      array[10] = 10;
+      // set the value of the index 
+      array[11] = 11;
+      // set the value of the index 
+      array[12] = 12;
+      // set the value of the index 
+      array[13] = 13;
+      // set the value of the index 
+      array[14] = 14;
+      // set the value of the index 
+      array[15] = 15;
+      // set the value of the index 
+      array[16] = 16;
+      // set the value of the index 
+      array[17] = 17;
+      // set the value of the index 
+      array[18] = 18;
+      // set the value of the index 
+      array[19] = 19;
+      // set the value of the index
+      array[20] = 20;
+      // set the value of the index 
+      array[21] = 21;
+       // set the value of the index
+      array[22] = 22;
+      // set the value of the index
+      array[23] = 23;
+      // set the value of the index
+      array[24] = 24;
+      // set the value of the index
+      array[25] = 25;
+      // set the value of the index
+      array[26] = 26;
+      // set the value of the index
+      array[27] = 27;
+      // set the value of the index
+      array[28] = 28;
+      // set the value of the index
+      array[29] = 29;
+      // set the value of the index
+      array[30] = 30;
+      // set the value of the index
+      array[31] = 31;
+      // set the value of the index
+      array[32] = 32;
+      // set the value of the index
+      array[33] = 33;
+      // set the value of the index
+      array[34] = 34;
+      // set the value of the index
+      array[35] = 35;
+      // set the value of the index
+      array[36] = 36;
+      // set the value of the index
+      array[37] = 37;
+      // set the value of the index
+      array[38] = 38;
+      // set the value of the index
+      array[39] = 39;
+      // set the value of the index
+      array[40] = 40;
+      // set the value of the index
+      array[41] = 41;
+      // set the value of the index
+      array[42] = 42;
+      // set the value of the index
+      array[43] = 43;
+      // set the value of the index
+      array[44] = 44;
+      // set the value of the index
+      array[45] = 45;
+      // set the value of the index
+      array[46] = 46;
+      // set the value of the index
+      array[47] = 47;
+      // set the value of the index
+      array[48] = 48;
+      // set the value of the index
+      array[49] = 49;
+      // set the value of the index
+      array[50] = 50;
+       // set the value of the index
+      array[51] = 51;
+      // set the value of the index
+      array[52] = 52;
+      // set the value of the index
+      array[53] = 53;
+      // set the value of the index
+      array[54] = 54;
+      // set the value of the index
+      array[55] = 55;
+      // set the value of the index
+      array[56] = 56;
+      // set the value of the index
+      array[57] = 57;
+      // set the value of the index
+      array[58] = 58;
+      // set the value of the index
+      array[59] = 59;
+      // set the value of the index
+      array[60] = 60;
+      // set the value of the index
+      array[61] = 61;
+      // set the value of the index
+      array[62] = 62;
+      // set the value of the index
+      array[63] = 63;
+      // set the value of the index
+      array[64] = 64;
+      // set the value of the index
+      array[65] = 65;
+      // set the value of the index
+      array[66] = 66;
+      // set the value of the index
+      array[67] = 67;
+      // set the value of the index
+      array[68] = 68;
+      // set the value of the index
+      array[69] = 69;
+      // set the value of the index
+      array[70] = 70;
+      // set the value of the index
+      array[71] = 72;
+      // set the value of the index
+      array[72] = 73;
+      // set the value of the index
+      array[73] = 74;
+      // set the value of the index
+      array[74] = 73;
+      // set the value of the index
+      array[75] = 74;
+      // set the value of the index
+      array[76] = 75;
+      // set the value of the index
+      array[76] = 76;
+      // set the value of the index
+      array[77] = 77;
+      // set the value of the index
+      array[78] = 78;
+      // set the value of the index
+      array[79] = 79;
+      // set the value of the index
+      array[80] = 80;
+      // set the value of the index
+      array[81] = 81;
+      // set the value of the index
+      array[82] = 82;
+      // set the value of the index
+      array[83] = 83;
+      // set the value of the index
+      array[84] = 84;
+      // set the value of the index
+      array[85] = 85;
+      // set the value of the index
+      array[86] = 86;
+      // set the value of the index
+      array[87] = 87;
+      // set the value of the index
+      array[88] = 88;
+      // set the value of the index
+      array[89] = 89;
+      // set the value of the index
+      array[90] = 90;
+      // set the value of the index
+      array[91] = 91;
+      // set the value of the index
+      array[92] = 92;
+      // set the value of the index
+      array[93] = 93;
+      // set the value of the index
+      array[94] = 94;
+      // set the value of the index
+      array[95] = 95;
+      // set the value of the index
+      array[96] = 96;
+      // set the value of the index
+      array[97] = 97;
+      // set the value of the index
+      array[98] = 98;
+      // set the value of the index    
+      array[99] = 99;
+      // set the value of the index    
+      array[100] = 100;
+      // set the value of the index
+      array[101] = 101;
+      // set the value of the index    
+      array[102] = 102;
+      // set the value of the index    
+      array[103] = 103;
+      // set the value of the index    
+      array[104] = 104;
+      // set the value of the index    
+      array[105] = 105;
+      // set the value of the index    
+      array[106] = 106;
+      // set the value of the index    
+      array[107] = 107;
+      // set the value of the index    
+      array[108] = 108;
+      // set the value of the index    
+      array[109] = 109;
+      // set the value of the index    
+      array[110] = 110;
+      // set the value of the index    
+      array[111] = 111;
+      // set the value of the index 
+      array[112] = 112;
+      // set the value of the index 
+      array[113] = 113;
+      // set the value of the index 
+      array[114] = 114;
+      // set the value of the index 
+      array[115] = 115;
+      // set the value of the index 
+      array[116] = 116;
+      // set the value of the index 
+      array[117] = 117;
+      // set the value of the index 
+      array[118] = 118;
+      // set the value of the index 
+      array[119] = 119;
+      // set the value of the index 
+      array[120] = 120;
+      // set the value of the index 
+      array[121] = 121;
+      // set the value of the index 
+      array[122] = 122;
+      // set the value of the index 
+      array[123] = 123;
+      // set the value of the index 
+      array[124] = 124;]
+      // set the value of the index 
+      array[125] = 125;
+      // set the value of the index 
+      array[126] = 126;
+      // set the value of the index 
+      array[127] = 127;
+      // set the value of the index 
+      array[128] = 128;
+      // set the value of the index 
+      array[129] = 129;
+      // set the value of the index 
+      array[130] = 130;
+      // set the value of the index 
+      array[131] = 131;
+      // set the value of the index 
+      array[132] = 132;
+      // set the value of the index 
+      array[133] = 133;
+      // set the value of the index 
+      array[134] = 134;
+      // set the value of the index 
+      array[135] = 135;
+      // set the value of the index 
+      array[136] = 136;
+      // set the value of the index 
+      array[137] = 137;
+      // set the value of the index 
+      array[138] = 138;
+      // set the value of the index 
+      array[139] = 139;
+      // set the value of the index 
+      array[140] = 140;
+      // set the value of the index 
+      array[141] = 141;
+      // set the value of the index 
+      array[142] = 142;
+      // set the value of the index 
+      array[143] = 143;
+      // set the value of the index 
+      array[144] = 144;
+      // set the value of the index 
+      array[145] = 145;
+      // set the value of the index 
+      array[146] = 146;
+      // set the value of the index 
+      array[147] = 147;
+      // set the value of the index 
+      array[148] = 148;
+      // set the value of the index 
+      array[149] = 149;
+      // set the value of the index 
+      array[150] = 150;
+      // set the value of the index 
+      array[151] = 151;
+      // set the value of the index 
+      array[152] = 152;
+      // set the value of the index 
+      array[153] = 153;
+      // set the value of the index 
+      array[154] = 154;
+      // set the value of the index 
+      array[155] = 155;
+      // set the value of the index 
+      array[156] = 156;
+      // set the value of the index 
+      array[157] = 157;
+      // set the value of the index 
+      array[158] = 158;
+      // set the value of the index 
+      array[159] = 159;
+      // set the value of the index 
+      array[160] = 160;
+      // set the value of the index 
+      array[161] = 161;
+      // set the value of the index 
+      array[162] = 162;
+      // set the value of the index 
+      array[163] = 163;
+      // set the value of the index 
+      array[164] = 164;
+      // set the value of the index 
+      array[165] = 165;
+      // set the value of the index 
+      array[166] = 166;
+      // set the value of the index 
+      array[166 = 166;
+      // set the value of the index 
+      array[167] = 167;
+      // set the value of the index 
+      array[168] = 168;
+      // set the value of the index 
+      array[169] = 169;
+      // set the value of the index 
+      array[170] = 170;      
+      // set the value of the index 
+      array[171] = 171;
+      // set the value of the index 
+      array[172] = 172;
+      // set the value of the index 
+      array[173] = 173;
+      // set the value of the index 
+      array[174] = 174;
+      // set the value of the index 
+      array[175] = 175;
+      // set the value of the index 
+      array[176] = 176;
+      // set the value of the index 
+      array[177] = 177;
+      // set the value of the index 
+      array[178] = 178;
+      // set the value of the index 
+      array[179] = 179;
+      // set the value of the index 
+      array[180] = 180;
+      // set the value of the index 
+      array[181] = 181;
+      // set the value of the index 
+      array[182] = 182;
+      // set the value of the index 
+      array[183] = 183;
+      // set the value of the index 
+      array[184] = 184;
+      // set the value of the index 
+      array[185] = 185;
+      // set the value of the index 
+      array[186] = 186;
+      // set the value of the index 
+      array[187] = 187;
+      // set the value of the index 
+      array[188] = 188;
+      // set the value of the index 
+      array[189] = 189;
+      // set the value of the index 
+      array[190] = 190;
+      // set the value of the index 
+      array[191] = 191;
+      // set the value of the index 
+      array[192] = 192;
+       // set the value of the index 
+      array[193] = 193;
+      // set the value of the index 
+      array[194] = 194;
+      // set the value of the index 
+      array[195] = 195;
+      // set the value of the index 
+      array[196] = 196;
+      // set the value of the index 
+      array[197] = 197;
+       // set the value of the index 
+      array[198] = 198;
+       // set the value of the index 
+      array[199] = 199;
+      // set the value of the index 
+      array[200] = 200;
+      // set the value of the index 
+      array[201] = 201;
+      // set the value of the index 
+      array[202] = 202;
+      // set the value of the index 
+      array[203] = 203;
+               // set the value of the index 
+      array[204] = 204;
+                      // 
+                // set the value of the index 
+      array[205] = 205;
+      
+            // set the value of the index 
       array[206] = 206;
+          
+                // set the value of the index 
+      array[207] = 206;
+            
+      // print the first element 
+      Console.WriteLine(array[0]);
+      // print the second element 
+      Console.WriteLine(array[1]);
+      // print the third element 
+      Console.WriteLine(array[2]);
+      // print the fourth element 
+      Console.WriteLine(array[3]);
+      // print the fifth element 
+      Console.WriteLine(array[4]);
+      // print the sixth element 
+      Console.WriteLine(array[5]);
+      // print the seventh element 
+      Console.WriteLine(array[6]);
+      // print the eighth element 
+      Console.WriteLine(array[7]);
+      // print the ninth element 
+      Console.WriteLine(array[8]);
+      // print the tenth element 
+      Console.WriteLine(array[9]);
+      // print the eleventh element 
+      Console.WriteLine(array[10]);
+      // print the twelth element 
+      Console.WriteLine(array[11]);
+      // print the thirteenth element 
+      Console.WriteLine(array[12]);
+      // print the fourteenth element 
+      Console.WriteLine(array[13]);
+      // print the fifteenth element 
+      Console.WriteLine(array[14]);
+      // print the sixteenth element
+      Console.WriteLine(array[15]);
+       // print the seventeenth element
+      Console.WriteLine(array[16]);
+      // print the eighteenth element
+      Console.WriteLine(array[17]);
+       // print the nineteenth element
+      Console.WriteLine(array[18]);
+      // print the twenieth element
+      Console.WriteLine(array[19]);
+      // print the twenty-first element
+      Console.WriteLine(array[20]);
+      // print the twenty-second element
+      Console.WriteLine(array[21]);
+      // print the twenty-third element
+      Console.WriteLine(array[22]);
+      // print the twenty-fourth element
+      Console.WriteLine(array[23]);
+      // print the twenty-fifth element
+      Console.WriteLine(array[24]);
+      // print the twenty-sixth element
+      Console.WriteLine(array[25]);
+      // print the twenty-seventh element
+      Console.WriteLine(arrray[26]);
+      // print the twenty-eighth element
+      Console.WriteLine(arrray[27]);
+      // print the twenty-ninth element
+      Console.WriteLine(arrray[28]);
+      // print the thirtieth element
+      Console.WriteLine(arrray[29]);
+      // print the thirty-first
+      Console.WriteLine(array[30]);
+      // print the thirty-second
+      Console.WriteLine(array[31]);
+      // print the thirty-third
+      Console.Writelibe(array[32]);
+      // print the thirty-fourth
+      Console.WriteLine(array[33]);
+      // print the thirty-fifth
+      Console.WriteLine(array[34]);
+      // print the thirty-sixth
+      Console.WriteLine(array[35]);
+      // print the thirty-seventh
+      Console.WriteLine(array[36]);
+      // print the thirty-eighth
+      Console.WriteLine(array[37]);
+      // print the thirty-ninth
+      Console.WriteLine(array[38]);
+      // print the fourtieth
+      Console.WriteLine(array[39]);
+      // print the forty-first
+      Console.WriteLine(array[40]);
+      // print the forty-second
+      Console.WriteLine(array[41]);
+      // print the forty-third
+      Console.WriteLine(array[42]);
+      // print the forty-fourth
+      Console.WriteLine(array[43]);
+      // print the forty-fifth
+      Console.WriteLine(array[44]);
+      // print the forty-sixth
+      Console.WriteLine(array[45]);
+      // print the forty-seventh
+      Console.WriteLine(array[46]);
+      // print the forty-eighth
+      Console.WriteLine(array[47]);
+      // print the forty-ninth
+      Console.WriteLine(array[48]);
+       // print the fifthieth
+      Console.WriteLine(array[49]);
+      // print the fifty-first
+      Console.WriteLine(array[50]);
+      // print the fifty-second
+      Console.WriteLine(array[51]);
+      // print the fifty-third
+      Console.WriteLine(array[52]);
+      // print the fifty-fourth
+      Console.WriteLine(array[53]);
+   }
+   // the end of program
+}
+
             
       // print the first element 
       Console.WriteLine(array[0]);
