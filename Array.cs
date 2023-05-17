e837f13cff09b81410420e2c87f1d94757a19c18
Lyons Software <steve.lyons@lyonssoftware.com>
2023-05-16 22:45:15 -0500

    Update Array.cs

--- Array.cs
+++ Array.cs
@@ -438,12 +438,12 @@ public class Array
       // set the value of the index 
       array[212] = 212;
       
-      
       // set the value of the index 
-      array[212] = 212;
+      array[213] = 212;
       
           // set the value of the index 
       array[214] = 214;
+      
      
             
             
@@ -859,138 +859,6 @@ public class Array
       array[198] = 198;
        // set the value of the index 
       array[199] = 199;
-      // set the value of the index 
-      array[200] = 200;
-      // set the value of the index 
-      array[201] = 201;
-      // set the value of the index 
-      array[202] = 202;
-      // set the value of the index 
-      array[203] = 203;
-               // set the value of the index 
-      array[204] = 204;
-                      // 
-                // set the value of the index 
-      array[205] = 205;
-      
-            // set the value of the index 
-      array[206] = 206;
-          
-                // set the value of the index 
-      array[207] = 206;
-            
-      // print the first element 
-      Console.WriteLine(array[0]);
-      // print the second element 
-      Console.WriteLine(array[1]);
-      // print the third element 
-      Console.WriteLine(array[2]);
-      // print the fourth element 
-      Console.WriteLine(array[3]);
-      // print the fifth element 
-      Console.WriteLine(array[4]);
-      // print the sixth element 
-      Console.WriteLine(array[5]);
-      // print the seventh element 
-      Console.WriteLine(array[6]);
-      // print the eighth element 
-      Console.WriteLine(array[7]);
-      // print the ninth element 
-      Console.WriteLine(array[8]);
-      // print the tenth element 
-      Console.WriteLine(array[9]);
-      // print the eleventh element 
-      Console.WriteLine(array[10]);
-      // print the twelth element 
-      Console.WriteLine(array[11]);
-      // print the thirteenth element 
-      Console.WriteLine(array[12]);
-      // print the fourteenth element 
-      Console.WriteLine(array[13]);
-      // print the fifteenth element 
-      Console.WriteLine(array[14]);
-      // print the sixteenth element
-      Console.WriteLine(array[15]);
-       // print the seventeenth element
-      Console.WriteLine(array[16]);
-      // print the eighteenth element
-      Console.WriteLine(array[17]);
-       // print the nineteenth element
-      Console.WriteLine(array[18]);
-      // print the twenieth element
-      Console.WriteLine(array[19]);
-      // print the twenty-first element
-      Console.WriteLine(array[20]);
-      // print the twenty-second element
-      Console.WriteLine(array[21]);
-      // print the twenty-third element
-      Console.WriteLine(array[22]);
-      // print the twenty-fourth element
-      Console.WriteLine(array[23]);
-      // print the twenty-fifth element
-      Console.WriteLine(array[24]);
-      // print the twenty-sixth element
-      Console.WriteLine(array[25]);
-      // print the twenty-seventh element
-      Console.WriteLine(arrray[26]);
-      // print the twenty-eighth element
-      Console.WriteLine(arrray[27]);
-      // print the twenty-ninth element
-      Console.WriteLine(arrray[28]);
-      // print the thirtieth element
-      Console.WriteLine(arrray[29]);
-      // print the thirty-first
-      Console.WriteLine(array[30]);
-      // print the thirty-second
-      Console.WriteLine(array[31]);
-      // print the thirty-third
-      Console.Writelibe(array[32]);
-      // print the thirty-fourth
-      Console.WriteLine(array[33]);
-      // print the thirty-fifth
-      Console.WriteLine(array[34]);
-      // print the thirty-sixth
-      Console.WriteLine(array[35]);
-      // print the thirty-seventh
-      Console.WriteLine(array[36]);
-      // print the thirty-eighth
-      Console.WriteLine(array[37]);
-      // print the thirty-ninth
-      Console.WriteLine(array[38]);
-      // print the fourtieth
-      Console.WriteLine(array[39]);
-      // print the forty-first
-      Console.WriteLine(array[40]);
-      // print the forty-second
-      Console.WriteLine(array[41]);
-      // print the forty-third
-      Console.WriteLine(array[42]);
-      // print the forty-fourth
-      Console.WriteLine(array[43]);
-      // print the forty-fifth
-      Console.WriteLine(array[44]);
-      // print the forty-sixth
-      Console.WriteLine(array[45]);
-      // print the forty-seventh
-      Console.WriteLine(array[46]);
-      // print the forty-eighth
-      Console.WriteLine(array[47]);
-      // print the forty-ninth
-      Console.WriteLine(array[48]);
-       // print the fifthieth
-      Console.WriteLine(array[49]);
-      // print the fifty-first
-      Console.WriteLine(array[50]);
-      // print the fifty-second
-      Console.WriteLine(array[51]);
-      // print the fifty-third
-      Console.WriteLine(array[52]);
-      // print the fifty-fourth
-      Console.WriteLine(array[53]);
-   }
-   // the end of program
-}
-
             
       // print the first element 
       Console.WriteLine(array[0]);
