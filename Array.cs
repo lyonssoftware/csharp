6228fbf3e6da4b5a9574a52afa76e83be3a6082a
Lyons Software <steve.lyons@lyonssoftware.com>
2023-05-27 23:44:21 -0500

    Update Array.cs

--- Array.cs
+++ Array.cs
@@ -426,6 +426,8 @@ public class Array
             
             array[205] = 205;
             
+            array[207 = 207;
+            
       // print the first element 
       Console.WriteLine(array[0]);
       // print the second element 
