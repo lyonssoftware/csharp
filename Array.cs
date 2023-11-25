019c55fb7496808ee5b59f2a165d3dbc46fda319
Lyons Software <steve.lyons@lyonssoftware.com>
2023-11-24 22:52:27 -0800

    Update Array.cs

--- Array.cs
+++ Array.cs
@@ -793,6 +793,7 @@ public class Array
       Console.WriteLine(array[123]);
       Console.WriteLine(array[124]); 
    Console.WriteLine(array[125]); 
+     Console.WriteLine(array[126]);       
    }
    // the end of program
 }
