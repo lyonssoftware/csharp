98f1afb2625eba006de494a2ec306337d54081c7
Lyons Software <steve.lyons@lyonssoftware.com>
2024-02-17 23:15:26 -0600

    Update Array.cs

--- Array.cs
+++ Array.cs
@@ -29,7 +29,6 @@ public class Array
       array[18] = 18;
       array[19] = 19;
       array[20] = 20;
-      // set the value of the index 
       array[21] = 21;
        // set the value of the index
       array[22] = 22;
