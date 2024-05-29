ffadd50077e050d9cb5b5e4c47c4a0bb0606a692
Lyons Software <steve.lyons@lyonssoftware.com>
2024-05-28 22:53:42 -0500

    Update DoWhile.cs

--- DoWhile.cs
+++ DoWhile.cs
@@ -7,15 +7,15 @@ public class DoWhile
    {
     // set the count to 3
     int x = 8;
-    int Y = 2;
-int d = 6;
+    int y = 10;
+    int d = 6;
       // execute the action at least once 
       do 
       {
          // add the increment 
         // increment the variable x by 1 
         x++;
-// new x count 
+        // new x count 
       }
       // while the x variable is less than 10 
      while(x < 10)
