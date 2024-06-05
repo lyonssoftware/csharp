08d187c9324c8e406a5ca36bc7ca0ec508535836
Lyons Software <steve.lyons@lyonssoftware.com>
2024-06-04 23:06:40 -0500

    Update Date.cs

--- Date.cs
+++ Date.cs
@@ -10,7 +10,8 @@ class Date
     // new format of the date 
     String.Format("{0:y yy yyy yyyy}", dt);  // "8 08 008 2008"   year
     // new format of the date with the date first
-     String.Format("{0:M MM MMM MMMM}", dt);  // "3 03 Mar March"  month
+    String.Format("{0:M MM MMM MMMM}", dt); 
+    // "3 03 Mar March"  month
     String.Format("{0:d dd ddd dddd}", dt);  // "9 09 Sun Sunday" day
     String.Format("{0:h hh H HH}",     dt);  // "4 04 16 16"      hour 12/24
     // format the minute 
