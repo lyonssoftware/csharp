3022d3e0fae6133d0702c01d609ce58f385b093a
Lyons Software <steve.lyons@lyonssoftware.com>
2025-01-07 22:13:00 -0600

    Update Addition.cs

--- Addition.cs
+++ Addition.cs
@@ -8,7 +8,185 @@ public class Addition
     int a = 15; 
     // initialize the int b
     int b = 20;
+    // initialize the int cf// program to add integers
+Using System;
+public class Addition
+{
+   public static void Main()
+   { 
+    // initialize the int a
+    int a = 15; 
+    // initialize the int b
+    int b = 20;
     // initialize the int c
+    int c = 40;
+    // initialize the int d
+    int d = 17;
+    // initialize the int e
+    int e = 2;
+    // initialize the int f
+    int f = 10;
+    // initialize the int g
+    int g = 12;
+    // initialize the int h
+    int h = 7;
+    // initialize the int i
+    int i = 7;
+    // initialize the int j
+    int j = 30;
+    // initialize the int k
+    int k = 23;
+    // initialize the int l
+    int l = 50;
+    // initialize the int m
+    int m = 2;
+    // initialize the int n
+    int n = 38;
+    // initialize the int o
+    int o = 89;
+    // initialize the int p
+    int p = 45;
+    // initialize the int n
+    int q = 45;
+    // initialize the int n
+    int r = 45;
+    // initialize the int n
+    int s = 72;
+    // initialize the int n
+    int t = 60;
+    // initialize the int n
+    int u = 8;
+    // initialize the int v
+    int v = 16;
+    // initialize the int 2
+    int w = 16;  
+    // initialize the int w
+    int x = 16;
+    int y = 32;
+    int z = 89;
+      
+      int aa = 10;
+      int bb = 23;
+      int cc = 39;
+      int dd = 30;
+      int ee = 75;
+      int ff = 100;
+      int gg = 50;
+      int hh = 89;
+      int ii = 56;
+      int jj = 24;
+      int kk = 15;
+      int ll = 10;
+      int mm = 20;
+      int nn = 30;
+      int oo = 0;
+      int pp = 2;
+      int qq = 29;
+      int rr = 20;
+      int ss = 70;
+      int tt = 28;
+      int uu = 61;
+      int vv = 32;
+      int ww = 32;
+      int xx = 60;
+      int yy = 98;
+      int zz = 90;
+      
+    // print the sum of 5 + 3 = 8
+    Console.WriteLine(a+b);
+    // print the sum of 7 + 2 = 9
+    Console.WriteLine(c+d);
+    // print the sum of 5 + 2 = 7
+    Console.Writeline(a+d);
+    // print the sum of e 
+    Console.Writeline(e);
+    // print the sum of f
+    Console.Writeline(f);
+    // print the sum of f
+    Console.Writeline(ff);
+    // print the sum of g
+   Console.Writeline(g);
+   // print the sum of 
+   Console.Writeline(gg);
+    // print the sum of 
+   Console.Writeline(h);
+   // print the sum of 
+   Console.Writeline(hh);
+   // print the sum of 
+   Console.Writeline(i);
+   // print the sum of 
+   Console.Writeline(ii);
+   // print the sum of 
+   Console.Writeline(j);
+   // print the sum of 
+   Console.Writeline(jj);
+   // print the sum of 
+   Console.Writeline(k);
+     // print the sum of 
+   Console.Writeline(kk);
+   // print the sum of 
+   Console.Writeline(l);
+   // print the sum of 
+   Console.Writeline(ll);
+  // print the sum of 
+  Console.Writeline(m);
+ // print the sum of 
+  Console.Writeline(mm);
+  // print the sum of 
+  Console.Writeline(n);
+  // print the sum of 
+  Console.Writeline(nn);
+  // print the sum of 
+  Console.Writeline(o);
+  // print the sum of 
+  Console.Writeline(oo);
+  // print the sum of 
+  Console.Writeline(p);
+  // print the sum of 
+  Console.Writeline(pp);
+  // print the sum of 
+  Console.Writeline(q);
+  // print the sum of 
+  Console.Writeline(qq);
+  // print the sum of 
+  Console.Writeline(r);
+  // print the sum of 
+  Console.Writeline(rr);
+  // print the sum of 
+  Console.Writeline(s);
+  // print the sum of 
+  Console.Writeline(ss);
+  // print the sum of 
+  Console.Writeline(t);
+  // print the sum of 
+  Console.Writeline(tt);
+  // print the sum of 
+  Console.Writeline(u);
+  // print the sum of 
+  Console.Writeline(uu);
+  // print the sum of 
+  Console.Writeline(v);
+  // print the sum of 
+  Console.Writeline(vv);
+  // print the sum of 
+  Console.Writeline(w);
+  // print the sum of 
+  Console.Writeline(ww);
+  // print the sum of 
+  Console.Writeline(x);
+   // print the sum of 
+  Console.Writeline(xx);
+  // print the sum of 
+  Console.Writeline(y);
+  // print the sum of 
+   Console.Writeline(yy);
+  // print the sum of 
+  Console.Writeline(z);
+  // print the sum of 
+  Console.Writeline(zz);
+  }
+} // end of the program
+
     int c = 40;
     // initialize the int d
     int d = 17;
