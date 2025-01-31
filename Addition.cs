9235fbea53b9b49456fb37e27d0364ac36ef9253
Lyons Software <steve.lyons@lyonssoftware.com>
2025-01-30 20:23:02 -0600

    Update Addition.cs

--- Addition.cs
+++ Addition.cs
@@ -4,34 +4,34 @@ public class Addition
 {
    public static void Main()
    { 
-    // initialize 
-    int a = 15; 
-    int b = 20;
-    int c = 40;
-    int d = 17;
-    int e = 2;
-    int f = 10;
-    int g = 12;
-    int h = 7;
-    int i = 7;x
-    int j = 30;
-    int k = 23;
-    int l = 50;
-    int m = 2;
-    int n = 38;
-    int o = 89;
-    int p = 45;
-    int q = 45;
-    int r = 45;
-    int s = 72;
-    int t = 60;
-    int u = 8;
-    int v = 16;
-    int w = 16;  
-    int x = 16;
-    int y = 32;
-    int z = 89;
-      
+      // initialize 
+       int a = 15; 
+       int b = 20;
+       int c = 40;
+       int d = 17;
+       int e = 2;
+       int f = 10;
+       int g = 12;
+       int h = 7;
+       int i = 7;x
+       int j = 30;
+       int k = 23;
+       int l = 50;
+       int m = 2;
+       int n = 38;
+       int o = 89;
+       int p = 45;
+       int q = 45;
+       int r = 45;
+       int s = 72;
+       int t = 60;
+       int u = 8;
+       int v = 16;
+       int w = 16;  
+       int x = 16;
+       int y = 32;
+       int z = 89;
+   
       int aa = 10;
       int bb = 23;
       int cc = 39;
