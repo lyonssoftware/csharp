20cdd80d6211576debfa3e08aad0e4e234910e6f
Lyons Software <steve.lyons@lyonssoftware.com>
2025-02-13 11:00:14 +0700

    Update Addition.cs

--- Addition.cs
+++ Addition.cs
@@ -12,34 +12,7 @@ public class Addition
        int e = 2;
        int f = 10;
        int g = 12;
-       int h = 7;
-       int i = 7;x
-       int j = 30;
-       int k = 23;
-       int l = 50;
-       int m = 2;
-       int n = 38;
-       int o = 89;
-       int p = 45;
-       int q = 45;
-       int r = 45;
-       int s = 72;
-       int t = 60;
-       int u = 8;
-       int v = 16;
-       int w = 16;  
-       int x = 16;
-       int y = 32;
-       int z = 89;
-   
-      int aa = 10;
-      int bb = 23;
-      int cc = 39;
-      int dd = 50;
-      int ee = 75;
-      int ff = 100;
-      int gg = 50;
-      int hh = 44;
+
 
       
     // print the sum of 5 + 3 = 8
