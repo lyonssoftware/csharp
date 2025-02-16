7e86cff9940c97c3883aca6151df318f2a9c2042
Lyons Software <steve.lyons@lyonssoftware.com>
2025-02-15 18:07:39 -0600

    Update Array.cs

--- Array.cs
+++ Array.cs
@@ -4,8 +4,8 @@ public class Array
 {
    public static void Main()
    {
-      // create an int array of 100 indexes 
-      int[] array = new int[100];
+      // create an int array of 20 indexes 
+      int[] array = new int[20];
       // add to the array
       array[0] = 0;
       array[1] = 1;
@@ -27,59 +27,6 @@ public class Array
       array[17] = 17;
       array[18] = 18;
       array[19] = 19;
-      array[20] = 20;
-      array[21] = 21;
-      array[22] = 22;
-      array[23] = 23;
-      array[24] = 24;
-      array[25] = 25;
-      array[26] = 26;
-      array[27] = 27;
-      array[28] = 28;
-      array[29] = 29;
-      array[30] = 30;
-      array[31] = 31;
-      array[32] = 32;
-      array[33] = 33;
-      array[34] = 34;
-      array[35] = 35;
-      array[36] = 36;
-      array[37] = 37;
-      array[38] = 38;
-      array[39] = 39;
-      array[40] = 40;
-      array[41] = 41;
-      array[42] = 42;
-      array[43] = 43;
-      array[44] = 44;
-      array[45] = 45;
-      array[46] = 46;
-      array[47] = 47;
-      array[48] = 48;
-      array[49] = 49;
-      array[50] = 50;
-      array[51] = 51;
-      array[52] = 52;
-      array[53] = 53;
-      array[54] = 54;
-      array[55] = 55;
-      array[56] = 56;
-      array[57] = 57;
-      array[58] = 58;
-      array[59] = 59;
-      array[60] = 60;
-      array[61] = 61;
-      array[62] = 62;
-      array[63] = 63;
-      array[64] = 64;
-      array[65] = 65;
-      array[66] = 66;
-      array[67] = 67;
-      array[68] = 68;
-      array[69] = 69;
-      array[70] = 70;
-      array[71] = 72;
-
       
       // print the items in array
       Console.WriteLine(array[0]);
@@ -100,86 +47,6 @@ public class Array
       Console.WriteLine(array[16]);
       Console.WriteLine(array[17]);
       Console.WriteLine(array[19]);
-      Console.WriteLine(array[20]);
-      Console.WriteLine(array[21]);
-      Console.WriteLine(array[22]);
-      Console.WriteLine(array[23]);
-      Console.WriteLine(array[24]);
-      Console.WriteLine(array[25]);
-      Console.WriteLine(array[26]);
-      Console.WriteLine(array[27]);
-      Console.WriteLine(array[28]);
-      Console.WriteLine(array[29]);
-      Console.WriteLine(array[30]);
-      Console.WriteLine(array[31]);
-      Console.Writelibe(array[32]);
-      Console.WriteLine(array[33]);
-      Console.WriteLine(array[34]);
-      Console.WriteLine(array[35]);
-      Console.WriteLine(array[36]);
-      Console.WriteLine(array[37]);
-      Console.WriteLine(array[38]);
-      Console.WriteLine(array[39]);
-      Console.WriteLine(array[40]);
-      Console.WriteLine(array[41]);
-      Console.WriteLine(array[42]);
-      Console.WriteLine(array[43]);
-      Console.WriteLine(array[44]);
-      Console.WriteLine(array[45]);
-      Console.WriteLine(array[46]);
-      Console.WriteLine(array[47]);
-      Console.WriteLine(array[48]);
-      Console.WriteLine(array[49]);
-      Console.WriteLine(array[50]);
-      Console.WriteLine(array[51]);
-      Console.WriteLine(array[52]);
-      Console.WriteLine(array[53]);
-      Console.WriteLine(array[54]);
-      Console.WriteLine(array[55]);
-      Console.WriteLine(array[56]);
-      Console.WriteLine(array[57]);
-      Console.WriteLine(array[58]);
-      Console.WriteLine(array[59]);
-      Console.WriteLine(array[60]);
-      Console.WriteLine(array[61]);
-      Console.WriteLine(array[62]);
-      Console.WriteLine(array[63]);
-      Console.WriteLine(array[64]);     
-      Console.WriteLine(array[65]);  
-      Console.WriteLine(array[66]); 
-      Console.WriteLine(array[67]); 
-      Console.WriteLine(array[68]); 
-      Console.WriteLine(array[69]);     
-      Console.WriteLine(array[70]); 
-      Console.WriteLine(array[71]); 
-      Console.WriteLine(array[72]); 
-      Console.WriteLine(array[73]); 
-      Console.WriteLine(array[74]); 
-      Console.WriteLine(array[75]); 
-      Console.WriteLine(array[76]);
-      Console.WriteLine(array[77]);
-      Console.WriteLine(array[78]); 
-      Console.WriteLine(array[79]);
-      Console.WriteLine(array[80]); 
-      Console.WriteLine(array[81]);
-      Console.WriteLine(array[82]);
-      Console.WriteLine(array[83]); 
-      Console.WriteLine(array[84]); 
-      Console.WriteLine(array[85]); 
-      Console.WriteLine(array[86]); 
-      Console.WriteLine(array[87]); 
-      Console.WriteLine(array[88]);
-      Console.WriteLine(array[89]); 
-      Console.WriteLine(array[90]); 
-      Console.WriteLine(array[91]); 
-      Console.WriteLine(array[92]);  
-      Console.WriteLine(array[93]); 
-      Console.WriteLine(array[94]); 
-      Console.WriteLine(array[95]); 
-      Console.WriteLine(array[96]);
-      Console.WriteLine(array[97]); 
-      Console.WriteLine(array[98]);
-      Console.WriteLine(array[99]);
    }
    // the end of program
 }
