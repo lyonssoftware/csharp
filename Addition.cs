651c9004bcbda96874dd6e3f1edf775975fa878d
Lyons Software <steve.lyons@lyonssoftware.com>
2025-04-14 22:22:49 -0500

    Update Addition.cs

--- Addition.cs
+++ Addition.cs
@@ -1,88 +1,57 @@
-// program to add integers
+// c#  of using arrays
 Using System;
-public class Addition
+public class Array
 {
    public static void Main()
-   { 
-      // initialize 
-       int a = 15; 
-       int b = 20;
-       int c = 40;
-       int d = 17;
-       int e = 2;
-       int f = 10;
-       int g = 16;
-       int h = 45;
-       int i = 73;
-       int j = 30;
-       int k = 11  
-       int l = 11;
-       int m = 10;
-       int n = 8;
-       int o = 45;
-       int p = 85;
-       int q = 55;
-       int r = 20;
-       int s = 20;
-       int t = 39;
-       int u = 53;
-       int v = 70;
-       int w = 39;
-       int x = 10;
-       int y = 10;
-       int z = 10;
+   {
+      // create an int array of 25 indexes 
+      int[] array = new int[25];
+      // add to the array
+      array[0] = 0;
+      array[1] = 1;
+      array[2] = 2;
+      array[3] = 3;
+      array[4] = 4;
+      array[5] = 5;
+      array[6] = 6; 
+      array[7] = 7; 
+      array[8] = 8;
+      array[9] = 9;
+      array[10] = 10;
+      array[11] = 11;
+      array[12] = 12;
+      array[13] = 13;
+      array[14] = 14;
+      array[15] = 15; 
+      array[16] = 16;
+      array[17] = 17;
+      array[18] = 18;
+      array[19] = 19;
+      array[20] = 19;
+      array[21] = 32;
+      array[22] = 32;
+      array[23] = 32;
+      array[24] = 32;
       
-       // print the sum of a and b
-       Console.WriteLine(a+b);
-       // print the sum 
-       Console.WriteLine(c+d);
-       // print the sum of c
-       Console.Writeline(c);
-       // print the sum of d 
-       Console.Writeline(d);
-       // print the sum of e
-       Console.Writeline(e);
-       // print the sum of f
-       Console.Writeline(f);
-       // print the sum of g
-       Console.Writeline(g);
-       // print the sum of h
-       Console.Writeline(h);
-       // print the sum of i
-       Console.Writeline(i);
-       // print the sum of j
-       Console.Writeline(j);
-       // print the sum of k
-       Console.Writeline(k);
-       // print the sum of l
-       Console.Writeline(l);
-       // print the sum of m
-       Console.Writeline(m);
-       // print the sum of n
-       Console.Writeline(n);
-       // print the sum of o
-       Console.Writeline(o);
-       // print the sum of p
-       Console.Writeline(p);
-      // print the sum of q
-       Console.Writeline(q);
-       // print the sum of r
-       Console.Writeline(r);
-      // print the sum of s
-       Console.Writeline(s);
-      // print the sum of t
-       Console.Writeline(t);
-      // print the sum of u
-       Console.Writeline(u);
-       // print the sum of v
-       Console.Writeline(v);
-       // print the sum of w
-       Console.Writeline(w);
-      // print the sum of x
-       Console.Writeline(x);
-       // print the sum of y
-       Console.Writeline(y);
-      // print the sum of z
-       Console.Writeline(z);
-  }
-} // end of the programx
+      // print the items in array
+      Console.WriteLine(array[0]);
+      Console.WriteLine(array[1]);
+      Console.WriteLine(array[3]);
+      Console.WriteLine(array[4]);
+      Console.WriteLine(array[5]);
+      Console.WriteLine(array[6]);
+      Console.WriteLine(array[7]);
+      Console.WriteLine(array[8]);
+      Console.WriteLine(array[9]);
+      Console.WriteLine(array[10]);
+      Console.WriteLine(array[11]);
+      Console.WriteLine(array[12]);
+      Console.WriteLine(array[13]);
+      Console.WriteLine(array[14]);
+      Console.WriteLine(array[15]);
+      Console.WriteLine(array[16]);
+      Console.WriteLine(array[17]);
+      Console.WriteLine(array[19]);
+   }
+   // the end of program
+}
