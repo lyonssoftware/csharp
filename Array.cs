612ed3a4379c5cc0899503e960b308ae1f011a9c
Lyons Software <steve.lyons@lyonssoftware.com>
2025-09-25 14:32:16 -1100

    Update Array.cs

--- Array.cs
+++ Array.cs
@@ -20,7 +20,7 @@ public class Array
       array[12] = 12;
       array[13] = 13;
       array[14] = 14;
-      array[15] = 15; 
+      array[15] = 47; 
       array[16] = 16;
       array[17] = 57;
       array[18] = 18;
