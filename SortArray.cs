2ab98382e26e27093459453743d73aa81d4303f3
Lyons Software <steve.lyons@lyonssoftware.com>
2026-01-17 17:45:07 -0900

    Update SortArray.cs

--- SortArray.cs
+++ SortArray.cs
@@ -9,10 +9,10 @@ public class SortArray
       int[] intArray = new int[5] { 8, 10, 2, 6, 3 };
       // make a second array 
       int[] intArray2 = new int[6] { 6, 9, 2, 6, 5, 10 };
-      
+      // 
       int[] intArray3 = new int[6] { 18, 10, 1, 6, 8, 11 };
-      
-      int[] intArray4 = new int[6] { 1, 8, 15, 6, 8, 1 };
+      // 
+      int[] intArray4 = new int[6] { 1, 20, 15, 6, 8, 18 };
       
        int[] intArray5 = new int[6] { 7, 8, 9, 6, 13, 1 };
       // sort the integer array object intArray
