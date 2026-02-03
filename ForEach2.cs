acb839a2ad9d4e8f18a251181745be1e1ea009b8
Lyons Software <steve.lyons@lyonssoftware.com>
2026-02-02 20:21:22 -0900

    Update ForEach2.cs

--- ForEach2.cs
+++ ForEach2.cs
@@ -5,18 +5,13 @@ public class ForEach2
    // main function of the program 
    public static void Main()
    {
-      // initialize the List with 3 names
+      // initialize the first list
       var names = new List<string>() { "John", "Tom", "Peter" };
        // initialize the second List 
       var names2 = new List<string>() { "Laura", "Sam", "Greg" };
-      // use foreach() to iterate through the names
-      
+      // initialize the third list
       var names3 = new List<string>() {"Sam", "Kim", "Don};
-
-      
-      var names2 = new List<string>() { "Laura", "Sam", "Greg" };
       // use foreach() to iterate through the names
- var names3 = new List<string>() { "Laura", "Sam", "Greg" };
       foreach (string name in names)
       {
        // print the name from the the List of strings
