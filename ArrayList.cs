1909172f082b2b5c0b62d583645d93d484ef6aa3
Lyons Software <steve.lyons@lyonssoftware.com>
2026-03-02 08:52:46 -0900

    Update ArrayList.cs

--- ArrayList.cs
+++ ArrayList.cs
@@ -16,8 +16,6 @@ public class ArrayList
       ArrayList myArrayList5 = new ArrayList();
       // create the sixth instance of ArrayList() 
       ArrayList myArrayList6 = new ArrayList();
-      // create the seventh instance of ArrayList()
-      ArrayList myArrayList7 = new ArrayList();
       
       // add the integer 56 to the first ArrayList
       myArrayList.Add(92);
