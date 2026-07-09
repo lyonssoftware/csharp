a53e0a2fa18bfd634a24e2ad77d22ae8fbed1c0e
Lyons Software <steve.lyons@lyonssoftware.com>
2026-07-09 13:37:33 -0800

    Update Addition.cs

--- Addition.cs
+++ Addition.cs
@@ -92,7 +92,6 @@ public class Array
       Console.WriteLine(array[44]);
       Console.WriteLine(array[45]);
       Console.WriteLine(array[46]);
-      Console.WriteLine(array[47]);
    }
    // the end of program
 }
